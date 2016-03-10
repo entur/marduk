ff71eb96d704be51f35ee189d637ce20f0714327
Tommy Gagnes <tommy.gagnes@knowit.no>
2016-03-10 13:12:53 +0100

    NRP-77 Specifying application/json in hubot notification

--- src/main/java/no/rutebanken/marduk/routes/otp/GraphBuiltNotificationRoute.java
+++ src/main/java/no/rutebanken/marduk/routes/otp/GraphBuiltNotificationRoute.java
@@ -1,6 +1,7 @@
 package no.rutebanken.marduk.routes.otp;
 
 import no.rutebanken.marduk.routes.BaseRouteBuilder;
+import org.apache.axis.transport.http.HTTPConstants;
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
 import org.springframework.beans.factory.annotation.Value;
@@ -35,6 +36,7 @@ public class GraphBuiltNotificationRoute extends BaseRouteBuilder {
                         .process(e -> e.getIn().setBody(new Metadata("Uploaded new Graph object file.", e.getIn().getHeader(FILE_HANDLE, String.class), new Date(), Metadata.Status.OK, Metadata.Action.OTP_GRAPH_UPLOAD).getJson()))
                         .removeHeaders("*")
                         .setHeader(Exchange.HTTP_METHOD, constant(org.apache.camel.component.http4.HttpMethods.GET))
+                        .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
                         .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
                         .toD("${property.notificationUrl}")
                         .log(LoggingLevel.DEBUG, getClass().getName(), "Done notifying. Got a ${header." + Exchange.HTTP_RESPONSE_CODE + "} back.")
