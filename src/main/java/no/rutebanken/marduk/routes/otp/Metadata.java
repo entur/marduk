be946d367ab8091b63b1c14c2689ec03fcde26e7
Tommy Gagnes <tommy.gagnes@knowit.no>
2016-04-05 11:11:36 +0200

    Adding timezone info to graph built notification

--- src/main/java/no/rutebanken/marduk/routes/otp/Metadata.java
+++ src/main/java/no/rutebanken/marduk/routes/otp/Metadata.java
@@ -18,7 +18,7 @@ public class Metadata {
     @JsonProperty("file_name")
     public String fileName;
 
-    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss", timezone="CET")
     @JsonProperty("date")
     public Date date;
 
