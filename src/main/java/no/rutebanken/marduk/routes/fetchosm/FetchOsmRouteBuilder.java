cbdd6184aa92164f3021399d35a7e9baa930a79e
Erlend Nossum <en@scienta.no>
2016-07-11 09:29:09 +0200

    NRP-475 Added MD5 check of read file

--- src/main/java/no/rutebanken/marduk/routes/fetchosm/FetchOsmRouteBuilder.java
+++ src/main/java/no/rutebanken/marduk/routes/fetchosm/FetchOsmRouteBuilder.java
@@ -1,8 +1,10 @@
 package no.rutebanken.marduk.routes.fetchosm;
 
+import no.rutebanken.marduk.exceptions.Md5ChecksumValidationException;
 import no.rutebanken.marduk.routes.BaseRouteBuilder;
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -28,6 +30,9 @@ import static no.rutebanken.marduk.Constants.FILE_HANDLE;
  */
 @Component
 public class FetchOsmRouteBuilder extends BaseRouteBuilder {
+
+    private static final String FILE_TARGET_MD5 = "targetMd5Sum";
+
     @Value("${fetch.osm.cron.schedule:0+*+*/23+?+*+MON-FRI}")
     private String cronSchedule;
 
@@ -42,14 +47,36 @@ public class FetchOsmRouteBuilder extends BaseRouteBuilder {
 
         from("direct:fetchOsmMapOverNorway")
                 .log(LoggingLevel.DEBUG, getClass().getName(), "Fetching OSM map over Norway.")
+                .to("direct:fetchOsmMapOverNorwayMd5")
                 .setHeader(Exchange.HTTP_METHOD, constant(org.apache.camel.component.http4.HttpMethods.GET))
                 .to( osmMapUrl )
+                .convertBodyTo(String.class)
+                .process(p -> {
+                    // Throw exception if the expected MD5 does not match MD5 from body
+                    String body = (String) p.getIn().getBody();
+                    String md5 = DigestUtils.md5Hex( body );
+                    String md5FromFile = (String) p.getIn().getHeader(FILE_TARGET_MD5);
+                    if (! md5.equals( md5FromFile)) {
+                        throw new Md5ChecksumValidationException("MD5 of body ("+md5+") does not match MD5 which was read from source ("+md5FromFile+")");
+                    }
+                })
                 .convertBodyTo(InputStream.class)
                 .setHeader(FILE_HANDLE, simple("osm/norway-latest.osm.pbf"))
                 .to("direct:uploadBlob")
                 .setBody(simple("File fetched, and blob store has been correctly updated"))
                 .log(LoggingLevel.DEBUG, getClass().getName(), "Processing of OSM map finished");
 
+        from("direct:fetchOsmMapOverNorwayMd5")
+                .log(LoggingLevel.DEBUG, getClass().getName(), "Fetching MD5 sum for map over")
+                .setHeader(Exchange.HTTP_METHOD, constant(org.apache.camel.component.http4.HttpMethods.GET))
+                .to( osmMapUrl+".md5" )
+                .convertBodyTo(String.class)
+                .process(p -> {
+                    String body = (String) p.getIn().getBody();
+                    String md5 = body.split(" ")[0];
+                    p.getOut().setHeader(FILE_TARGET_MD5, md5);
+                })
+                .log(LoggingLevel.DEBUG, getClass().getName(), "MD5 sum fetched and set in header");
 
         // One time per 24H on MON-FRI
         from("quartz2://marduk/fetchOsmMap?cron="+cronSchedule+"&trigger.timeZone=Europe/Oslo")
