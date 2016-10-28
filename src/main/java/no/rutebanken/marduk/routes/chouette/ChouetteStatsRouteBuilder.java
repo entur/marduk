c96170f315a31ffc57e6c27a5695a58d3da95a10
Tommy Gagnes <tommy.gagnes@knowit.no>
2016-10-28 10:09:31 +0200

    NRP-702 Making validity categories configurable

--- src/main/java/no/rutebanken/marduk/routes/chouette/ChouetteStatsRouteBuilder.java
+++ src/main/java/no/rutebanken/marduk/routes/chouette/ChouetteStatsRouteBuilder.java
@@ -1,8 +1,13 @@
 package no.rutebanken.marduk.routes.chouette;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.component.http4.HttpMethods;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import static no.rutebanken.marduk.Constants.CHOUETTE_REFERENTIAL;
 import static no.rutebanken.marduk.Constants.PROVIDER_ID;
@@ -14,6 +19,9 @@ public class ChouetteStatsRouteBuilder extends AbstractChouetteRouteBuilder {
     @Value("${chouette.url}")
     private String chouetteUrl;
 
+    @Value("${chouette.stats.validity.categories}")
+    private String[] validityCategories;
+
     @Override
     public void configure() throws Exception {
         super.configure();
@@ -22,11 +30,14 @@ public class ChouetteStatsRouteBuilder extends AbstractChouetteRouteBuilder {
                 .process( e -> e.getIn().setHeader(CHOUETTE_REFERENTIAL, getProviderRepository().getProvider(e.getIn().getHeader(PROVIDER_ID, Long.class)).chouetteInfo.referential))
                 .removeHeaders("Camel*")
                 .setBody(constant(""))
-                .setHeader(Exchange.HTTP_METHOD, constant(org.apache.camel.component.http4.HttpMethods.GET))
-                // TODO validity categories should be extracted as a config parameter
-                .setProperty("chouette_url", simple(chouetteUrl + "/chouette_iev/statistics/${header." + CHOUETTE_REFERENTIAL + "}/line?minDaysValidityCategory=120&minDaysValidityCategory=127"))
+                .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.GET))
+                .setProperty("chouette_url", simple(chouetteUrl + "/chouette_iev/statistics/${header." + CHOUETTE_REFERENTIAL + "}/line?" + getValidityCategories()))
+                .log(LoggingLevel.DEBUG, getClass().getName(), correlation() + "Calling chouette with ${property.chouette_url}")
                 .toD("${exchangeProperty.chouette_url}");
     }
 
+    private String getValidityCategories() {
+        return Arrays.asList(validityCategories).stream().map(s -> "minDaysValidityCategory=" + s).collect(Collectors.joining("&"));
+    }
 
 }
