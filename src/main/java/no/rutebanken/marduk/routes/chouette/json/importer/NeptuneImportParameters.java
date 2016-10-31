8a4b8fbccd4436462d06430b8b8da617a0bfb6d6
Tommy Gagnes <tommy.gagnes@knowit.no>
2016-10-31 22:43:47 +0100

    NRP-703 Adding update_stop_places import parameter

--- src/main/java/no/rutebanken/marduk/routes/chouette/json/importer/NeptuneImportParameters.java
+++ src/main/java/no/rutebanken/marduk/routes/chouette/json/importer/NeptuneImportParameters.java
@@ -24,6 +24,7 @@ public class NeptuneImportParameters extends ChouetteJobParameters {
         neptuneImport.organisationName = organisationName;
         neptuneImport.userName = userName;
         neptuneImport.cleanRepository = cleanRepository ? "1":"0";
+        neptuneImport.updateStopPlaces = false;
         Parameters parameters = new Parameters();
         parameters.neptuneImport = neptuneImport;
         NeptuneImportParameters neptuneImportParameters = new NeptuneImportParameters();
