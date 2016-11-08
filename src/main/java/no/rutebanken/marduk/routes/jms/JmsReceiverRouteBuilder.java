61c4e991fc756c0b858e61afcd3a408af3501a5e
Tommy Gagnes <tommy.gagnes@knowit.no>
2016-11-08 15:24:34 +0100

    NRP-758 Fixed file name bug

--- src/main/java/no/rutebanken/marduk/routes/jms/JmsReceiverRouteBuilder.java
+++ src/main/java/no/rutebanken/marduk/routes/jms/JmsReceiverRouteBuilder.java
@@ -21,7 +21,7 @@ public class JmsReceiverRouteBuilder extends BaseRouteBuilder {
 
         from("activemq:queue:MardukInboundQueue?transacted=true")
             .transacted()
-            .log(LoggingLevel.INFO, correlation()+"Received notification about file ${header.CamelFileName} on jms. Fetching file ...")
+            .log(LoggingLevel.INFO, correlation()+"Received notification about file '${header." + Constants.FILE_NAME + "}' on jms. Fetching file ...")
                 .log(LoggingLevel.INFO, correlation() + "Fetching blob ${header." + FILE_HANDLE + "}")
                 .to("direct:fetchExternalBlob")
                 .process(
@@ -30,7 +30,7 @@ public class JmsReceiverRouteBuilder extends BaseRouteBuilder {
             .log(LoggingLevel.INFO, correlation() + "File handle is: ${header." + FILE_HANDLE + "}")
             .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
             .to("direct:uploadBlob")
-            .setHeader(Constants.FILE_NAME, exchangeProperty(Exchange.FILE_NAME))
+            .setHeader(Constants.FILE_NAME, exchangeProperty(Constants.FILE_NAME))
             .process(e -> Status.builder(e).action(Status.Action.FILE_TRANSFER).state(Status.State.STARTED).build())
             .to("direct:updateStatus")
             .to("direct:deleteExternalBlob")
