901a5341c1e23ca299930df8a6d6aba15d905542
Tommy Gagnes <tommy.gagnes@knowit.no>
2016-11-28 13:41:27 +0100

    Revert "Improve workaround bean header issue"
    This reverts commit 61d92c2320f2bf99afa62eb5356c6a350ee9be3b.

--- src/main/java/no/rutebanken/marduk/routes/blobstore/ExternalBlobStoreRoute.java
+++ src/main/java/no/rutebanken/marduk/routes/blobstore/ExternalBlobStoreRoute.java
@@ -6,6 +6,8 @@ import org.apache.camel.LoggingLevel;
 import org.springframework.stereotype.Component;
 
 import static no.rutebanken.marduk.Constants.FILE_HANDLE;
+import static no.rutebanken.marduk.Constants.FILE_NAME;
+import static no.rutebanken.marduk.Constants.PROVIDER_ID;
 
 @Component
 public class ExternalBlobStoreRoute extends BaseRouteBuilder {
@@ -13,16 +15,26 @@ public class ExternalBlobStoreRoute extends BaseRouteBuilder {
     @Override
     public void configure() throws Exception {
         from("direct:fetchExternalBlob")
+                .setProperty(FILE_NAME, header(FILE_NAME))
+                .setProperty(FILE_HANDLE, header(FILE_HANDLE))
+                .setProperty(PROVIDER_ID, header(PROVIDER_ID))
                 .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
-                .process(e -> e.getIn().setHeaders(e.getIn().getHeaders()))
                 .bean("exchangeBlobStoreService","getBlob")
+                .setHeader(FILE_HANDLE, exchangeProperty(FILE_HANDLE))
+                .setHeader(FILE_NAME, exchangeProperty(FILE_NAME))
+                .setHeader(PROVIDER_ID, exchangeProperty(PROVIDER_ID))
                 .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true");
 
         from("direct:deleteExternalBlob")
                 .log(LoggingLevel.INFO, correlation() + "Deleting blob ${header." + FILE_HANDLE + "} from blob store.")
+                .setProperty(FILE_NAME, header(FILE_NAME))
+                .setProperty(FILE_HANDLE, header(FILE_HANDLE))
+                .setProperty(PROVIDER_ID, header(PROVIDER_ID))
                 .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
-                .process(e -> e.getOut().setHeaders(e.getIn().getHeaders()))
                 .bean("exchangeBlobStoreService","deleteBlob")
+                .setHeader(FILE_HANDLE, exchangeProperty(FILE_HANDLE))
+                .setHeader(FILE_NAME, exchangeProperty(FILE_NAME))
+                .setHeader(PROVIDER_ID, exchangeProperty(PROVIDER_ID))
                 .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true");
 
     }
