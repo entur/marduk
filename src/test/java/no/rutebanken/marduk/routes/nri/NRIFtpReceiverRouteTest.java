effa720b25c3b3c822b7786e59e90406d0056f54
Tommy Gagnes <tommy.gagnes@knowit.no>
2016-11-28 10:38:59 +0100

    Revert "NRP-704 Sleeping until fake FTP server is started"
    This reverts commit 25a6b5473f26e8c4d87e5f5fe9e58bc0fdeed4c1.

--- src/test/java/no/rutebanken/marduk/routes/nri/NRIFtpReceiverRouteTest.java
+++ src/test/java/no/rutebanken/marduk/routes/nri/NRIFtpReceiverRouteTest.java
@@ -12,6 +12,7 @@ import org.apache.camel.test.spring.CamelSpringRunner;
 import org.apache.camel.test.spring.UseAdviceWith;
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockftpserver.fake.FakeFtpServer;
@@ -20,8 +21,6 @@ import org.mockftpserver.fake.filesystem.DirectoryEntry;
 import org.mockftpserver.fake.filesystem.FileEntry;
 import org.mockftpserver.fake.filesystem.FileSystem;
 import org.mockftpserver.fake.filesystem.UnixFakeFileSystem;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -41,9 +40,9 @@ import static org.mockito.Mockito.when;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @ActiveProfiles({ "default", "dev" })
 @UseAdviceWith
+@Ignore
+// TODO Tommy is going to look at this test
 public class NRIFtpReceiverRouteTest extends MardukRouteBuilderIntegrationTestBase {
-
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private ModelCamelContext context;
@@ -91,11 +90,6 @@ public class NRIFtpReceiverRouteTest extends MardukRouteBuilderIntegrationTestBa
 		fakeFtpServer.setServerControlPort(32220);
 
 		fakeFtpServer.start();
-
-		while (!fakeFtpServer.isStarted()){
-			logger.info("Fake FTP not started. Sleeping 1 sec.");
-			Thread.sleep(1000);
-		}
 
 		context.getRouteDefinitions().get(0).adviceWith(context, new AdviceWithRouteBuilder() {
 			@Override
