0919ed90e472b6adae7b74291a3d6e8b8442f599
Tommy Gagnes <tommy.gagnes@knowit.no>
2016-12-22 13:09:28 +0100

    Support restarts/kills

--- src/main/java/no/rutebanken/marduk/routes/BaseRouteBuilder.java
+++ src/main/java/no/rutebanken/marduk/routes/BaseRouteBuilder.java
@@ -2,20 +2,24 @@ package no.rutebanken.marduk.routes;
 
 import no.rutebanken.marduk.Constants;
 import no.rutebanken.marduk.repository.ProviderRepository;
-import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.spring.SpringRouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 /**
  * Defines common route behavior.
  */
-public abstract class BaseRouteBuilder extends RouteBuilder {
+public abstract class BaseRouteBuilder extends SpringRouteBuilder {
 
     @Autowired
     private ProviderRepository providerRepository;
 
     @Override
     public void configure() throws Exception {
+
+        errorHandler(transactionErrorHandler()
+                .logExhausted(true)
+                .logRetryStackTrace(true));
     }
 
     protected ProviderRepository getProviderRepository(){
