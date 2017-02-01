a5f7fc93bc55e1211d88c529e9f3789d7abd9856
erlendnils1 <erlendnils1@gmail.com>
2017-02-01 08:56:02 +0100

    NRP-1154 Filter out files without file name from listFlatBlobs

--- src/main/java/no/rutebanken/marduk/repository/GcsBlobStoreRepository.java
+++ src/main/java/no/rutebanken/marduk/repository/GcsBlobStoreRepository.java
@@ -4,6 +4,7 @@ import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.Storage;
 import no.rutebanken.marduk.domain.BlobStoreFiles;
+import org.apache.commons.lang3.StringUtils;
 import org.rutebanken.helper.gcp.BlobStoreHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,56 +21,59 @@ import java.util.Iterator;
 @Scope("prototype")
 public class GcsBlobStoreRepository implements BlobStoreRepository {
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private Storage storage;
+	private Storage storage;
 
-    private String containerName;
+	private String containerName;
 
-    @Override
-    public void setStorage(Storage storage) {
-        this.storage = storage;
-    }
+	@Override
+	public void setStorage(Storage storage) {
+		this.storage = storage;
+	}
 
-    @Override
-    public void setContainerName(String containerName) {
-        this.containerName = containerName;
-    }
+	@Override
+	public void setContainerName(String containerName) {
+		this.containerName = containerName;
+	}
 
-    @Override
-    public BlobStoreFiles listBlobs(String prefix) {
-        Iterator<Blob> blobIterator = BlobStoreHelper.listAllBlobsRecursively(storage, containerName, prefix);
-        BlobStoreFiles blobStoreFiles = new BlobStoreFiles();
-        while (blobIterator.hasNext()) {
-            Blob blob = blobIterator.next();
-            blobStoreFiles.add(new BlobStoreFiles.File(blob.getName(), new Date(blob.getUpdateTime()), blob.getSize()));
-        }
-        return blobStoreFiles;
-    }
+	@Override
+	public BlobStoreFiles listBlobs(String prefix) {
+		Iterator<Blob> blobIterator = BlobStoreHelper.listAllBlobsRecursively(storage, containerName, prefix);
+		BlobStoreFiles blobStoreFiles = new BlobStoreFiles();
+		while (blobIterator.hasNext()) {
+			Blob blob = blobIterator.next();
+			blobStoreFiles.add(new BlobStoreFiles.File(blob.getName(), new Date(blob.getUpdateTime()), blob.getSize()));
+		}
+		return blobStoreFiles;
+	}
 
-    @Override
-    public BlobStoreFiles listBlobsFlat(String prefix) {
-        Iterator<Blob> blobIterator = BlobStoreHelper.listAllBlobsRecursively(storage, containerName, prefix);
-        BlobStoreFiles blobStoreFiles = new BlobStoreFiles();
-        while (blobIterator.hasNext()) {
-            Blob blob = blobIterator.next();
-            blobStoreFiles.add(new BlobStoreFiles.File(blob.getName().replace(prefix, ""), new Date(blob.getUpdateTime()), blob.getSize()));
-        }
-        return blobStoreFiles;
-    }
+	@Override
+	public BlobStoreFiles listBlobsFlat(String prefix) {
+		Iterator<Blob> blobIterator = BlobStoreHelper.listAllBlobsRecursively(storage, containerName, prefix);
+		BlobStoreFiles blobStoreFiles = new BlobStoreFiles();
+		while (blobIterator.hasNext()) {
+			Blob blob = blobIterator.next();
+			String fileName = blob.getName().replace(prefix, "");
+			if (!StringUtils.isEmpty(fileName)) {
+				blobStoreFiles.add(new BlobStoreFiles.File(fileName, new Date(blob.getUpdateTime()), blob.getSize()));
+			}
+		}
+		return blobStoreFiles;
+	}
 
-    @Override
-    public InputStream getBlob(String name) {
-        return BlobStoreHelper.getBlob(storage, containerName, name);
-    }
+	@Override
+	public InputStream getBlob(String name) {
+		return BlobStoreHelper.getBlob(storage, containerName, name);
+	}
 
-    @Override
-    public void uploadBlob(String name, InputStream inputStream, boolean makePublic) {
-        BlobStoreHelper.uploadBlob(storage, containerName, name, inputStream, makePublic);
-    }
+	@Override
+	public void uploadBlob(String name, InputStream inputStream, boolean makePublic) {
+		BlobStoreHelper.uploadBlob(storage, containerName, name, inputStream, makePublic);
+	}
 
-    @Override
-    public boolean delete(String objectName) {
-        return BlobStoreHelper.delete(storage, BlobId.of(containerName, objectName));
-    }
+	@Override
+	public boolean delete(String objectName) {
+		return BlobStoreHelper.delete(storage, BlobId.of(containerName, objectName));
+	}
 }
