df64677a5a285695890de0fff9bc177a70f6da7b
erlendnils1 <erlendnils1@gmail.com>
2017-03-09 09:14:46 +0100

    Slight refactoring + test

--- src/main/java/no/rutebanken/marduk/geocoder/routes/pelias/mapper/PeliasIndexParentInfoEnricher.java
+++ src/main/java/no/rutebanken/marduk/geocoder/routes/pelias/mapper/PeliasIndexParentInfoEnricher.java
@@ -2,6 +2,7 @@ package no.rutebanken.marduk.geocoder.routes.pelias.mapper;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
 import no.rutebanken.marduk.geocoder.GeoCoderConstants;
 import no.rutebanken.marduk.geocoder.geojson.KartverketLocality;
 import no.rutebanken.marduk.geocoder.routes.pelias.elasticsearch.ElasticsearchCommand;
@@ -33,23 +34,12 @@ public class PeliasIndexParentInfoEnricher {
 			return;
 		}
 		PeliasDocument peliasDocument = (PeliasDocument) command.getSource();
+		addParentIdsByReverseGeoLookup(adminUnitRepository, peliasDocument);
+		addAdminUnitNamesByIds(adminUnitRepository, peliasDocument);
+	}
+
+	private void addAdminUnitNamesByIds(AdminUnitRepository adminUnitRepository, PeliasDocument peliasDocument) {
 		Parent parent = peliasDocument.getParent();
-
-
-		GeoPoint centerPoint = peliasDocument.getCenterPoint();
-		if ((parent == null || parent.getLocalityId() == null) && centerPoint != null) {
-			KartverketLocality locality = adminUnitRepository.getLocality(new GeometryFactory().createPoint(new Coordinate(centerPoint.getLon(), centerPoint.getLat())));
-			if (locality != null) {
-				if (parent == null) {
-					parent = new Parent();
-					peliasDocument.setParent(parent);
-				}
-				parent.setLocalityId(locality.getId());
-				parent.setCountyId(locality.getParentId());
-			}
-		}
-
-
 		if (parent != null) {
 			if (parent.getCountyId() != null && parent.getCounty() == null) {
 				parent.setCounty(adminUnitRepository.getAdminUnitName(parent.getCountyId()));
@@ -60,9 +50,30 @@ public class PeliasIndexParentInfoEnricher {
 			if (parent.getBoroughId() != null && parent.getBorough() == null) {
 				parent.setBorough(adminUnitRepository.getAdminUnitName(parent.getBoroughId()));
 			}
-
-
 		}
+	}
+
+	private Parent addParentIdsByReverseGeoLookup(AdminUnitRepository adminUnitRepository, PeliasDocument peliasDocument) {
+		Parent parent = peliasDocument.getParent();
+
+		GeoPoint centerPoint = peliasDocument.getCenterPoint();
+		if (isLocalityMissing(parent) && centerPoint != null) {
+			Point point = geometryFactory.createPoint(new Coordinate(centerPoint.getLon(), centerPoint.getLat()));
+			KartverketLocality locality = adminUnitRepository.getLocality(point);
+			if (locality != null) {
+				if (parent == null) {
+					parent = new Parent();
+					peliasDocument.setParent(parent);
+				}
+				parent.setLocalityId(locality.getId());
+				parent.setCountyId(locality.getParentId());
+			}
+		}
+		return parent;
+	}
+
+	private boolean isLocalityMissing(Parent parent) {
+		return parent == null || parent.getLocalityId() == null;
 	}
 
 }
---
+++ src/test/java/no/rutebanken/marduk/geocoder/routes/pelias/mapper/PeliasIndexParentInfoEnricherTest.java
@@ -0,0 +1,52 @@
+package no.rutebanken.marduk.geocoder.routes.pelias.mapper;
+
+import com.vividsolutions.jts.geom.Point;
+import no.rutebanken.marduk.geocoder.geojson.KartverketLocality;
+import no.rutebanken.marduk.geocoder.routes.pelias.elasticsearch.ElasticsearchCommand;
+import no.rutebanken.marduk.geocoder.routes.pelias.json.GeoPoint;
+import no.rutebanken.marduk.geocoder.routes.pelias.json.PeliasDocument;
+import no.rutebanken.marduk.geocoder.services.AdminUnitRepository;
+import org.geotools.feature.simple.SimpleFeatureImpl;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.opengis.feature.simple.SimpleFeature;
+
+public class PeliasIndexParentInfoEnricherTest {
+
+	@Mock
+	private AdminUnitRepository adminUnitRepository;
+
+	@Mock
+	private KartverketLocality locality;
+
+	private PeliasIndexParentInfoEnricher parentInfoEnricher = new PeliasIndexParentInfoEnricher();
+
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void testAddParentInfoByReverseGeoLookup() {
+		PeliasDocument doc = new PeliasDocument("l", "s", "sid");
+		doc.setCenterPoint(new GeoPoint(1.0, 2.0));
+		ElasticsearchCommand command = ElasticsearchCommand.peliasIndexCommand(doc);
+
+		Mockito.when(adminUnitRepository.getLocality(Mockito.any(Point.class))).thenReturn(locality);
+		Mockito.when(locality.getId()).thenReturn("0103");
+		Mockito.when(locality.getParentId()).thenReturn("01");
+
+		Mockito.when(adminUnitRepository.getAdminUnitName("0103")).thenReturn("GokkLocality");
+		Mockito.when(adminUnitRepository.getAdminUnitName("01")).thenReturn("GokkCounty");
+
+		parentInfoEnricher.addMissingParentInfo(command, adminUnitRepository);
+
+		Assert.assertEquals("GokkCounty", doc.getParent().getCounty());
+		Assert.assertEquals("GokkLocality", doc.getParent().getLocality());
+	}
+}
