36b12f588829f323aace66bedf40f5e3b901e1a1
erlendnils1 <erlendnils1@gmail.com>
2017-03-31 12:47:13 +0200

    Set srsname and dimensions for polygons

--- src/main/java/no/rutebanken/marduk/geocoder/netex/NetexGeoUtil.java
+++ src/main/java/no/rutebanken/marduk/geocoder/netex/NetexGeoUtil.java
@@ -3,33 +3,33 @@ package no.rutebanken.marduk.geocoder.netex;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Polygon;
-import net.opengis.gml._3.AbstractRingPropertyType;
-import net.opengis.gml._3.DirectPositionListType;
-import net.opengis.gml._3.LinearRingType;
-import net.opengis.gml._3.PolygonType;
+import net.opengis.gml._3.*;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
 public class NetexGeoUtil {
 
 
-	public static PolygonType toNetexPolygon(Polygon polygon) {
-		LinearRingType linearRing = new LinearRingType();
+    public static final String SRS_NAME_WGS84 = "ESPG:4326";
 
-		List<Double> values = new ArrayList<>();
-		for (Coordinate coordinate : polygon.getExteriorRing().getCoordinates()) {
-			values.add(coordinate.y); // lat
-			values.add(coordinate.x); // lon
-		}
+    public static PolygonType toNetexPolygon(Polygon polygon) {
+        LinearRingType linearRing = new LinearRingType();
 
-		// Ignoring interior rings because the corresponding exclaves are not handled.
+        List<Double> values = new ArrayList<>();
+        for (Coordinate coordinate : polygon.getExteriorRing().getCoordinates()) {
+            values.add(coordinate.y); // lat
+            values.add(coordinate.x); // lon
+        }
 
-		DirectPositionListType positionList = new DirectPositionListType().withValue(values);
-		linearRing.withPosList(positionList);
+        // Ignoring interior rings because the corresponding exclaves are not handled.
 
-		return new PolygonType()
-				       .withExterior(new AbstractRingPropertyType().withAbstractRing(
-						       new net.opengis.gml._3.ObjectFactory().createLinearRing(linearRing)));
-	}
+        DirectPositionListType positionList = new DirectPositionListType().withValue(values);
+        linearRing.withPosList(positionList);
+
+        return new PolygonType().withSrsDimension(BigInteger.valueOf(2)).withSrsName(SRS_NAME_WGS84)
+                       .withExterior(new AbstractRingPropertyType().withAbstractRing(
+                               new net.opengis.gml._3.ObjectFactory().createLinearRing(linearRing)));
+    }
 }
