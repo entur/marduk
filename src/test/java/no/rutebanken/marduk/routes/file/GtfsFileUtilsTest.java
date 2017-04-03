21d336e63ebb60a5fd50f23f7d5994a8958fded6
erlendnils1 <erlendnils1@gmail.com>
2017-04-03 12:27:57 +0200

    NRP-1339 - Test to verify replacement of ':' in GTFS ids with '.' to please OTP

--- src/test/java/no/rutebanken/marduk/routes/file/GtfsFileUtilsTest.java
+++ src/test/java/no/rutebanken/marduk/routes/file/GtfsFileUtilsTest.java
@@ -1,42 +1,55 @@
 package no.rutebanken.marduk.routes.file;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.Arrays;
+import java.util.List;
 
 public class GtfsFileUtilsTest {
 
-	private static final String GTFS_FILE_1 = "src/test/resources/no/rutebanken/marduk/routes/file/beans/gtfs.zip";
-	private static final String GTFS_FILE_2 = "src/test/resources/no/rutebanken/marduk/routes/file/beans/gtfs2.zip";
+    private static final String GTFS_FILE_1 = "src/test/resources/no/rutebanken/marduk/routes/file/beans/gtfs.zip";
+    private static final String GTFS_FILE_2 = "src/test/resources/no/rutebanken/marduk/routes/file/beans/gtfs2.zip";
 
-	@Test
-	public void mergeGtfsFiles_identicalFilesShouldYieldMergedFileIdenticalToOrg() throws Exception {
+    @Test
+    public void mergeGtfsFiles_identicalFilesShouldYieldMergedFileIdenticalToOrg() throws Exception {
 
-		File input1 = new File(GTFS_FILE_1);
-		File merged = GtfsFileUtils.mergeGtfsFiles(Arrays.asList(input1, input1));
+        File input1 = new File(GTFS_FILE_1);
+        File merged = GtfsFileUtils.mergeGtfsFiles(Arrays.asList(input1, input1));
 
-		// Should assert content, but no exceptions must do for now
-		// Assert.assertTrue(FileUtils.sizeOf(merged) <= FileUtils.sizeOf(input1));
+        // Should assert content, but no exceptions must do for now
+        // Assert.assertTrue(FileUtils.sizeOf(merged) <= FileUtils.sizeOf(input1));
 
-		Assert.assertTrue(new ZipFileUtils().listFilesInZip(merged).stream().anyMatch(n -> "feed_info.txt" .equals(n)));
-	}
+        Assert.assertTrue(new ZipFileUtils().listFilesInZip(merged).stream().anyMatch(n -> "feed_info.txt".equals(n)));
+    }
 
-	@Test
-	public void mergeGtfsFiles_nonIdenticalFilesShouldYieldUnion() throws Exception {
+    @Test
+    public void mergeGtfsFiles_nonIdenticalFilesShouldYieldUnion() throws Exception {
 
-		File input1 = new File(GTFS_FILE_1);
-		File input2 = new File(GTFS_FILE_2);
-		File merged = GtfsFileUtils.mergeGtfsFiles(Arrays.asList(input1, input2));
+        File input1 = new File(GTFS_FILE_1);
+        File input2 = new File(GTFS_FILE_2);
+        File merged = GtfsFileUtils.mergeGtfsFiles(Arrays.asList(input1, input2));
 
-		Assert.assertTrue(FileUtils.sizeOf(merged) >= FileUtils.sizeOf(input1));
-		Assert.assertTrue(FileUtils.sizeOf(merged) >= FileUtils.sizeOf(input2));
+        Assert.assertTrue(FileUtils.sizeOf(merged) >= FileUtils.sizeOf(input1));
+        Assert.assertTrue(FileUtils.sizeOf(merged) >= FileUtils.sizeOf(input2));
 
-		Assert.assertTrue(new ZipFileUtils().listFilesInZip(merged).stream().anyMatch(n -> "feed_info.txt" .equals(n)));
-	}
+        Assert.assertTrue(new ZipFileUtils().listFilesInZip(merged).stream().anyMatch(n -> "feed_info.txt".equals(n)));
+    }
 
 
+    @Test
+    public void replaceIdSeparatorInFile() throws Exception {
+        File out = GtfsFileUtils.transformIdsToOTPFormat(new File(GTFS_FILE_2));
+
+        List<String> lines = IOUtils.readLines(new ByteArrayInputStream(ZipFileUtils.extractFileFromZipFile(new FileInputStream(out), "stops.txt").toByteArray()));
+
+        Assert.assertEquals("RUT.StopArea.7600100,Oslo S,59.910200,10.755330,RUT.StopArea.7600207", lines.get(1));
+
+    }
 
 }
