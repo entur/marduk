260644cdb53563fed95546b832ab4366dbc16dab
erlendnils1 <erlendnils1@gmail.com>
2017-04-04 09:09:27 +0200

    Tmp check to remove invalid message circulating in carbon

--- src/main/java/no/rutebanken/marduk/routes/chouette/ChouetteExportGtfsRouteBuilder.java
+++ src/main/java/no/rutebanken/marduk/routes/chouette/ChouetteExportGtfsRouteBuilder.java
@@ -25,102 +25,107 @@ import static no.rutebanken.marduk.Utils.getLastPathElementOfUrl;
 @Component
 public class ChouetteExportGtfsRouteBuilder extends AbstractChouetteRouteBuilder {
 
-	@Value("${chouette.url}")
-	private String chouetteUrl;
+    @Value("${chouette.url}")
+    private String chouetteUrl;
 
-	@Value("${chouette.export.days.forward:365}")
-	private int daysForward;
+    @Value("${chouette.export.days.forward:365}")
+    private int daysForward;
 
-	@Value("${chouette.export.days.back:365}")
-	private int daysBack;
+    @Value("${chouette.export.days.back:365}")
+    private int daysBack;
 
-	@Override
-	public void configure() throws Exception {
-		super.configure();
+    @Override
+    public void configure() throws Exception {
+        super.configure();
 
-		from("activemq:queue:ChouetteExportQueue?transacted=true").streamCaching()
-				.transacted()
-				.log(LoggingLevel.INFO, getClass().getName(), "Starting Chouette GTFS export for provider with id ${header." + PROVIDER_ID + "}")
-				.process(e -> {
-					// Add correlation id only if missing
-					e.getIn().setHeader(Constants.CORRELATION_ID, e.getIn().getHeader(Constants.CORRELATION_ID, UUID.randomUUID().toString()));
-					e.getIn().removeHeader(Constants.CHOUETTE_JOB_ID);
-				})
-				.process(e -> Status.builder(e).action(Action.EXPORT).state(State.PENDING).build())
-				.to("direct:updateStatus")
+        from("activemq:queue:ChouetteExportQueue?transacted=true").streamCaching()
+                .transacted()
+                .log(LoggingLevel.INFO, getClass().getName(), "Starting Chouette GTFS export for provider with id ${header." + PROVIDER_ID + "}")
+                .process(e -> {
+                    // Add correlation id only if missing
+                    e.getIn().setHeader(Constants.CORRELATION_ID, e.getIn().getHeader(Constants.CORRELATION_ID, UUID.randomUUID().toString()));
+                    e.getIn().removeHeader(Constants.CHOUETTE_JOB_ID);
+                })
+                .process(e -> Status.builder(e).action(Action.EXPORT).state(State.PENDING).build())
+                .to("direct:updateStatus")
+                .choice()
+                .when(e -> getProviderRepository().getProvider(e.getIn().getHeader(PROVIDER_ID, Long.class)) == null)
+                .log(LoggingLevel.WARN, "Removed export for unknown provider")
+                .stop()
+                .end()
 
-				.process(e -> e.getIn().setHeader(CHOUETTE_REFERENTIAL, getProviderRepository().getProvider(e.getIn().getHeader(PROVIDER_ID, Long.class)).chouetteInfo.referential))
-				.process(e -> e.getIn().setHeader(JSON_PART, Parameters.getGtfsExportParameters(getProviderRepository().getProvider(e.getIn().getHeader(PROVIDER_ID, Long.class))))) //Using header to addToExchange json data
-				.log(LoggingLevel.INFO, correlation() + "Creating multipart request")
-				.to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
-				.process(e -> toGenericChouetteMultipart(e))
-				.to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
-				.setHeader(Exchange.CONTENT_TYPE, simple("multipart/form-data"))
-				.to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
-				.toD(chouetteUrl + "/chouette_iev/referentials/${header." + CHOUETTE_REFERENTIAL + "}/exporter/gtfs")
-				.to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
-				.process(e -> {
-					e.getIn().setHeader(CHOUETTE_JOB_STATUS_URL, e.getIn().getHeader("Location").toString().replaceFirst("http", "http4"));
-					e.getIn().setHeader(Constants.CHOUETTE_JOB_ID, getLastPathElementOfUrl(e.getIn().getHeader("Location", String.class)));
-				})
-				.setHeader(Constants.CHOUETTE_JOB_STATUS_ROUTING_DESTINATION, constant("direct:processExportResult"))
-				.setHeader(Constants.CHOUETTE_JOB_STATUS_JOB_TYPE, constant(Action.EXPORT.name()))
-				.removeHeader("loopCounter")
-				.to("activemq:queue:ChouettePollStatusQueue")
-				.routeId("chouette-send-export-job");
+                .process(e -> e.getIn().setHeader(CHOUETTE_REFERENTIAL, getProviderRepository().getProvider(e.getIn().getHeader(PROVIDER_ID, Long.class)).chouetteInfo.referential))
+                .process(e -> e.getIn().setHeader(JSON_PART, Parameters.getGtfsExportParameters(getProviderRepository().getProvider(e.getIn().getHeader(PROVIDER_ID, Long.class))))) //Using header to addToExchange json data
+                .log(LoggingLevel.INFO, correlation() + "Creating multipart request")
+                .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
+                .process(e -> toGenericChouetteMultipart(e))
+                .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
+                .setHeader(Exchange.CONTENT_TYPE, simple("multipart/form-data"))
+                .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
+                .toD(chouetteUrl + "/chouette_iev/referentials/${header." + CHOUETTE_REFERENTIAL + "}/exporter/gtfs")
+                .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
+                .process(e -> {
+                    e.getIn().setHeader(CHOUETTE_JOB_STATUS_URL, e.getIn().getHeader("Location").toString().replaceFirst("http", "http4"));
+                    e.getIn().setHeader(Constants.CHOUETTE_JOB_ID, getLastPathElementOfUrl(e.getIn().getHeader("Location", String.class)));
+                })
+                .setHeader(Constants.CHOUETTE_JOB_STATUS_ROUTING_DESTINATION, constant("direct:processExportResult"))
+                .setHeader(Constants.CHOUETTE_JOB_STATUS_JOB_TYPE, constant(Action.EXPORT.name()))
+                .removeHeader("loopCounter")
+                .to("activemq:queue:ChouettePollStatusQueue")
+                .routeId("chouette-send-export-job");
 
 
-		from("direct:processExportResult")
-				.to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
-				.choice()
-				.when(simple("${header.action_report_result} == 'OK'"))
-				.log(LoggingLevel.INFO, correlation() + "Export ok")
-				.log(LoggingLevel.DEBUG, correlation() + "Calling url ${header.data_url}")
-				.removeHeaders("Camel*")
-				.setBody(simple(""))
-				.setHeader(Exchange.HTTP_METHOD, constant(org.apache.camel.component.http4.HttpMethods.GET))
-				.toD("${header.data_url}")
-				.to("direct:addGtfsFeedInfo")
-				.setHeader(BLOBSTORE_MAKE_BLOB_PUBLIC, constant(true))
-				.setHeader(FILE_HANDLE, simple(BLOBSTORE_PATH_OUTBOUND + "gtfs/${header." + CHOUETTE_REFERENTIAL + "}-" + Constants.CURRENT_AGGREGATED_GTFS_FILENAME))
-				.to("direct:uploadBlob")
-				.process(e -> Status.builder(e).action(Action.EXPORT).state(State.OK).build()).to("direct:updateStatus")
-				.removeHeader(Constants.CHOUETTE_JOB_ID)
-				.setBody(constant(null))
-				.to("activemq:queue:OtpGraphQueue")
-				.to("activemq:queue:ChouetteExportNetexQueue")
-				.process(e -> Status.builder(e).action(Action.BUILD_GRAPH).state(State.PENDING).build())
-				.when(simple("${header.action_report_result} == 'NOK'"))
-				.log(LoggingLevel.WARN, correlation() + "Export failed")
-				.process(e -> Status.builder(e).action(Action.EXPORT).state(State.FAILED).build())
-				.otherwise()
-				.log(LoggingLevel.ERROR, correlation() + "Something went wrong on export")
-				.process(e -> Status.builder(e).action(Action.EXPORT).state(State.FAILED).build())
-				.end()
-				.to("direct:updateStatus")
-				.routeId("chouette-process-export-status");
+        from("direct:processExportResult")
+                .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
+                .choice()
+                .when(simple("${header.action_report_result} == 'OK'"))
+                .log(LoggingLevel.INFO, correlation() + "Export ok")
+                .log(LoggingLevel.DEBUG, correlation() + "Calling url ${header.data_url}")
+                .removeHeaders("Camel*")
+                .setBody(simple(""))
+                .setHeader(Exchange.HTTP_METHOD, constant(org.apache.camel.component.http4.HttpMethods.GET))
+                .toD("${header.data_url}")
+                .to("direct:addGtfsFeedInfo")
+                .setHeader(BLOBSTORE_MAKE_BLOB_PUBLIC, constant(true))
+                .setHeader(FILE_HANDLE, simple(BLOBSTORE_PATH_OUTBOUND + "gtfs/${header." + CHOUETTE_REFERENTIAL + "}-" + Constants.CURRENT_AGGREGATED_GTFS_FILENAME))
+                .to("direct:uploadBlob")
+                .process(e -> Status.builder(e).action(Action.EXPORT).state(State.OK).build()).to("direct:updateStatus")
+                .removeHeader(Constants.CHOUETTE_JOB_ID)
+                .setBody(constant(null))
+                .to("activemq:queue:OtpGraphQueue")
+                .to("activemq:queue:ChouetteExportNetexQueue")
+                .process(e -> Status.builder(e).action(Action.BUILD_GRAPH).state(State.PENDING).build())
+                .when(simple("${header.action_report_result} == 'NOK'"))
+                .log(LoggingLevel.WARN, correlation() + "Export failed")
+                .process(e -> Status.builder(e).action(Action.EXPORT).state(State.FAILED).build())
+                .otherwise()
+                .log(LoggingLevel.ERROR, correlation() + "Something went wrong on export")
+                .process(e -> Status.builder(e).action(Action.EXPORT).state(State.FAILED).build())
+                .end()
+                .to("direct:updateStatus")
+                .routeId("chouette-process-export-status");
 
-		from("direct:addGtfsFeedInfo")
-				.log(LoggingLevel.INFO, correlation() + "Adding feed_info.txt to GTFS file")
-				.process(e -> {
-					// Add feed info
-					String feedInfoContent = "feed_id,feed_publisher_name,feed_publisher_url,feed_lang\nRB,Rutebanken,http://www.rutebanken.org,no";
+        from("direct:addGtfsFeedInfo")
+                .log(LoggingLevel.INFO, correlation() + "Adding feed_info.txt to GTFS file")
+                .process(e -> {
+                    // Add feed info
+                    String feedInfoContent = "feed_id,feed_publisher_name,feed_publisher_url,feed_lang\nRB,Rutebanken,http://www.rutebanken.org,no";
 
-					File tmpFolder = new File(System.getProperty("java.io.tmpdir"));
-					File tmpFolder2 = new File(tmpFolder, UUID.randomUUID().toString());
-					tmpFolder2.mkdirs();
-					File feedInfoFile = new File(tmpFolder2, "feed_info.txt");
+                    File tmpFolder = new File(System.getProperty("java.io.tmpdir"));
+                    File tmpFolder2 = new File(tmpFolder, UUID.randomUUID().toString());
+                    tmpFolder2.mkdirs();
+                    File feedInfoFile = new File(tmpFolder2, "feed_info.txt");
 
-					PrintWriter writer = new PrintWriter(feedInfoFile);
-					writer.write(feedInfoContent);
-					writer.close();
+                    PrintWriter writer = new PrintWriter(feedInfoFile);
+                    writer.write(feedInfoContent);
+                    writer.close();
 
-					e.getIn().setBody(ZipFileUtils.addFilesToZip(e.getIn().getBody(InputStream.class), new File[]{feedInfoFile}));
-					feedInfoFile.delete();
-					tmpFolder2.delete();
-				})
-				.routeId("chouette-process-export-gtfs-feedinfo");
-	}
+                    e.getIn().setBody(ZipFileUtils.addFilesToZip(e.getIn().getBody(InputStream.class), new File[]{feedInfoFile}));
+                    feedInfoFile.delete();
+                    tmpFolder2.delete();
+                })
+                .routeId("chouette-process-export-gtfs-feedinfo");
+    }
 
 
 }
