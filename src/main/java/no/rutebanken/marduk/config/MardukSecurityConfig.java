5853e20c4cf8eb414606a1b90f8a7b373368474c
erlendnils1 <erlendnils1@gmail.com>
2017-04-19 16:10:21 +0200

    (Somewhat hacky) solution to support keycloak access tokens with spring security

--- src/main/java/no/rutebanken/marduk/config/MardukSecurityConfig.java
+++ src/main/java/no/rutebanken/marduk/config/MardukSecurityConfig.java
@@ -5,6 +5,7 @@ import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter.AUTHORIZATION_HEADER;
 
 
 @Configuration
@@ -50,6 +59,35 @@ public class MardukSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     public KeycloakConfigResolver KeycloakConfigResolver() {
         return new KeycloakSpringBootConfigResolver();
     }
+
+
+    /**
+     * Override KeycloakAuthenticationProcessingFilter to support token as query param.
+     *
+     * Must:
+     * - Add request matcher to enable the filter for requests that contain 'access_token' query parameter
+     * - Treat such requests as those containing token as Authorization header. Thus is (somewhat dirty) achieved by overriding method
+     * for checking whether request is of bearer token type to include query param token requests.
+     *
+     */
+    @Override
+    @Bean
+    protected KeycloakAuthenticationProcessingFilter keycloakAuthenticationProcessingFilter() throws Exception {
+        final RegexRequestMatcher tokenQueryParamMatcher = new RegexRequestMatcher("(.*?)access_token=(.*?)", null);
+        RequestMatcher requestMatcher =
+                new OrRequestMatcher(new RequestHeaderRequestMatcher(AUTHORIZATION_HEADER), tokenQueryParamMatcher);
+
+        KeycloakAuthenticationProcessingFilter filter = new KeycloakAuthenticationProcessingFilter(authenticationManagerBean(), requestMatcher) {
+            @Override
+            protected boolean isBearerTokenRequest(HttpServletRequest request) {
+                return super.isBearerTokenRequest(request) || tokenQueryParamMatcher.matches(request);
+            }
+        };
+
+        filter.setSessionAuthenticationStrategy(sessionAuthenticationStrategy());
+        return filter;
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
