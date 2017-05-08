89e72d13b66fbd93bede6e8ecfcdad7dc0cb2167
erlendnils1 <erlendnils1@gmail.com>
2017-05-08 21:20:19 +0200

    NRP-1505 Create pelias documents representing streets

--- src/test/java/no/rutebanken/marduk/geocoder/routes/pelias/PeliasUpdateESIndexRouteIntegrationTest.java
+++ src/test/java/no/rutebanken/marduk/geocoder/routes/pelias/PeliasUpdateESIndexRouteIntegrationTest.java
@@ -10,75 +10,83 @@ import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.model.ModelCamelContext;
-import org.apache.camel.test.spring.CamelSpringRunner;
-import org.apache.camel.test.spring.UseAdviceWith;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.io.File;
 import java.io.FileInputStream;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,classes = PeliasUpdateEsIndexRouteBuilder.class, properties = "spring.main.sources=no.rutebanken.marduk.test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = PeliasUpdateEsIndexRouteBuilder.class, properties = "spring.main.sources=no.rutebanken.marduk.test")
 public class PeliasUpdateESIndexRouteIntegrationTest extends MardukRouteBuilderIntegrationTestBase {
 
-	@Autowired
-	private ModelCamelContext context;
+    @Autowired
+    private ModelCamelContext context;
 
-	@Value("${elasticsearch.scratch.url:http4://es-scratch:9200}")
-	private String elasticsearchScratchUrl;
+    @Value("${elasticsearch.scratch.url:http4://es-scratch:9200}")
+    private String elasticsearchScratchUrl;
 
-	@Value("${tiamat.export.blobstore.subdirectory:tiamat}")
-	private String blobStoreSubdirectoryForTiamatExport;
+    @Value("${tiamat.export.blobstore.subdirectory:tiamat}")
+    private String blobStoreSubdirectoryForTiamatExport;
 
-	@Value("${kartverket.blobstore.subdirectory:kartverket}")
-	private String blobStoreSubdirectoryForKartverket;
+    @Value("${kartverket.blobstore.subdirectory:kartverket}")
+    private String blobStoreSubdirectoryForKartverket;
 
-	@EndpointInject(uri = "mock:es-scratch")
-	protected MockEndpoint esScratchMock;
+    @EndpointInject(uri = "mock:es-scratch-bulk-index")
+    protected MockEndpoint esScratchMock;
 
-	@Produce(uri = "direct:insertElasticsearchIndexData")
-	protected ProducerTemplate insertESDataTemplate;
-	@Autowired
-	private InMemoryBlobStoreRepository inMemoryBlobStoreRepository;
+    @EndpointInject(uri = "mock:es-scratch-address-search")
+    protected MockEndpoint esScratchAddressSearchMock;
+
+    @Produce(uri = "direct:insertElasticsearchIndexData")
+    protected ProducerTemplate insertESDataTemplate;
+    @Autowired
+    private InMemoryBlobStoreRepository inMemoryBlobStoreRepository;
+
+    private static final String ADDRESS_SEARCH_RESPONSE = "{\"hits\":{\"total\":48,\"hits\":[{\"_index\":\"pelias\",\"_type\":\"address\",\"_id\":\"219137805\",\"_score\":12.170439,\"_source\":{\"center_point\":{\"lon\":8.085754906437227,\"lat\":58.175904836789755},\"parent\":{\"county_id\":[\"10\"],\"locality_id\":[\"1001\"],\"postalCode_id\":[\"4635\"],\"county\":[\"Vest-Agder\"],\"locality\":[\"Kristiansand\"],\"borough_id\":[\"10011703\"],\"borough\":[\"Hånes Vest\"],\"country_id\":[\"NOR\"]},\"name\":{\"default\":\"Vigvollåsen 39\"},\"alpha3\":\"NOR\",\"address_parts\":{\"zip\":\"4635\",\"number\":\"39\",\"street\":\"Vigvollåsen\",\"name\":\"Vigvollåsen\"},\"source\":\"kartverket\",\"source_id\":\"219137805\",\"category\":[\"Vegadresse\"],\"defaultName\":\"Vigvollåsen 39\",\"layer\":\"address\"}}]}}";
+
+    @Test
+    public void testInsertElasticsearchIndexDataSuccess() throws Exception {
+
+        // Stub for elastic search scratch instance
+        context.getRouteDefinition("pelias-invoke-bulk-command").adviceWith(context, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                interceptSendToEndpoint(elasticsearchScratchUrl + "/_bulk")
+                        .skipSendToOriginalEndpoint().to("mock:es-scratch-bulk-index");
+            }
+        });
+
+        context.getRouteDefinition("pelias-create-street-from-addresses").adviceWith(context, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                interceptSendToEndpoint(elasticsearchScratchUrl + "/pelias/address/_search")
+                        .skipSendToOriginalEndpoint().to("mock:es-scratch-address-search");
+            }
+        });
+
+        inMemoryBlobStoreRepository.uploadBlob(blobStoreSubdirectoryForKartverket + "/administrativeUnits/SosiTest.sos",
+                new FileInputStream(new File("src/test/resources/no/rutebanken/marduk/geocoder/sosi/SosiTest.sos")), false);
+        inMemoryBlobStoreRepository.uploadBlob(blobStoreSubdirectoryForKartverket + "/placeNames/placenames.geojson",
+                new FileInputStream(new File("src/test/resources/no/rutebanken/marduk/geocoder/geojson/fylker.geojson")), false);
+        inMemoryBlobStoreRepository.uploadBlob(blobStoreSubdirectoryForKartverket + "/addresses/addresses.csv",
+                new FileInputStream(new File("src/test/resources/no/rutebanken/marduk/geocoder/csv/addresses.csv")), false);
+        inMemoryBlobStoreRepository.uploadBlob(blobStoreSubdirectoryForTiamatExport + "/tiamat/tiamat-export-latest.xml",
+                new FileInputStream(new File("src/test/resources/no/rutebanken/marduk/geocoder/netex/tiamat-export.xml")), false);
 
 
-	@Test
-	public void testInsertElasticsearchIndexDataSuccess() throws Exception {
+        esScratchMock.expectedMessageCount(5);
 
-		// Stub for elastic search scratch instance
-		context.getRouteDefinition("pelias-invoke-bulk-command").adviceWith(context, new AdviceWithRouteBuilder() {
-			@Override
-			public void configure() throws Exception {
-				interceptSendToEndpoint(elasticsearchScratchUrl + "/_bulk")
-						.skipSendToOriginalEndpoint().to("mock:es-scratch");
-			}
-		});
+        esScratchAddressSearchMock.whenExchangeReceived(1, e -> e.getIn().setBody(ADDRESS_SEARCH_RESPONSE));
+        context.start();
 
-		inMemoryBlobStoreRepository.uploadBlob(blobStoreSubdirectoryForKartverket + "/administrativeUnits/SosiTest.sos",
-				new FileInputStream(new File("src/test/resources/no/rutebanken/marduk/geocoder/sosi/SosiTest.sos")), false);
-		inMemoryBlobStoreRepository.uploadBlob(blobStoreSubdirectoryForKartverket + "/placeNames/placenames.geojson",
-				new FileInputStream(new File("src/test/resources/no/rutebanken/marduk/geocoder/geojson/fylker.geojson")), false);
-		inMemoryBlobStoreRepository.uploadBlob(blobStoreSubdirectoryForKartverket + "/addresses/addresses.csv",
-				new FileInputStream(new File("src/test/resources/no/rutebanken/marduk/geocoder/csv/addresses.csv")), false);
-		inMemoryBlobStoreRepository.uploadBlob(blobStoreSubdirectoryForTiamatExport + "/tiamat/tiamat-export-latest.xml",
-				new FileInputStream(new File("src/test/resources/no/rutebanken/marduk/geocoder/netex/tiamat-export.xml")), false);
+        Exchange e = insertESDataTemplate.request("direct:insertElasticsearchIndexData", ex -> {
+        });
 
+        Assert.assertEquals(GeoCoderConstants.PELIAS_ES_SCRATCH_STOP, e.getProperty(GeoCoderConstants.GEOCODER_NEXT_TASK));
+        esScratchMock.assertIsSatisfied();
+    }
 
-
-		esScratchMock.expectedMessageCount(4);
-
-		context.start();
-
-		Exchange e = insertESDataTemplate.request("direct:insertElasticsearchIndexData", ex -> {
-		});
-
-		Assert.assertEquals(GeoCoderConstants.PELIAS_ES_SCRATCH_STOP, e.getProperty(GeoCoderConstants.GEOCODER_NEXT_TASK));
-		esScratchMock.assertIsSatisfied();
-	}
-}
+    }
