5976c3e8090926e8de068c24d08391c3b7754c52
erlendnils1 <erlendnils1@gmail.com>
2017-05-19 11:35:11 +0200

    Reject too small files from kartverket. setting limit to 1000 bytes

--- src/main/java/no/rutebanken/marduk/geocoder/services/KartverketService.java
+++ src/main/java/no/rutebanken/marduk/geocoder/services/KartverketService.java
@@ -5,10 +5,13 @@ import no.jskdata.Downloader;
 import no.jskdata.GeoNorgeDownloadAPI;
 import no.jskdata.KartverketDownload;
 import no.rutebanken.marduk.Constants;
+import no.rutebanken.marduk.exceptions.MardukException;
 import org.apache.camel.Exchange;
 import org.apache.camel.Header;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -21,65 +24,75 @@ import java.util.UUID;
 
 @Service
 public class KartverketService {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    @Value("${kartverket.username}")
+    private String username;
 
-	@Value("${kartverket.username}")
-	private String username;
+    @Value("${kartverket.password}")
+    private String password;
 
-	@Value("${kartverket.password}")
-	private String password;
+    @Value("${kartverket.file.min.bytes:1000}")
+    private int minBytes;
+
+    public List<File> downloadFiles(@Header(value = Constants.KARTVERKET_DATASETID) String dataSetId,
+                                           @Header(value = Constants.KARTVERKET_FORMAT) String format,
+                                           @Header(value = Exchange.FILE_PARENT) String localDownloadDir) {
+        Downloader kd = getDownloader(dataSetId, format);
+
+        return downloadFilesInternal(dataSetId, kd, localDownloadDir);
+    }
 
 
-	public List<File> downloadFiles(@Header(value = Constants.KARTVERKET_DATASETID) String dataSetId,
-			                               @Header(value = Constants.KARTVERKET_FORMAT) String format,
-			                               @Header(value = Exchange.FILE_PARENT) String localDownloadDir) {
-		Downloader kd = getDownloader(dataSetId, format);
+    private List<File> downloadFilesInternal(String dataSetId, Downloader kd, String localDownloadDir) {
+        List<File> files = new ArrayList<>();
+        try {
+            kd.login();
+            kd.dataset(dataSetId);
+            kd.download(new DefaultReceiver() {
 
-		return downloadFilesInternal(dataSetId, kd, localDownloadDir);
-	}
+                @Override
+                public void receive(String fileName, InputStream in) throws IOException {
+                    File file = new File(localDownloadDir + "/" + fileName);
 
+                    byte[] content = IOUtils.toByteArray(in);
+                    if (minBytes > 0 && content.length < minBytes) {
 
-	private List<File> downloadFilesInternal(String dataSetId, Downloader kd, String localDownloadDir) {
-		List<File> files = new ArrayList<>();
-		try {
-			kd.login();
-			kd.dataset(dataSetId);
-			kd.download(new DefaultReceiver() {
+                        logger.warn("Got too small file from kartverket, assuming it is an error message: " + new String(content));
+                        throw new MardukException("File content fetched from Kartverket was less than accepted minimum. Probably error message. Filename: " + fileName);
+                    }
 
-				@Override
-				public void receive(String fileName, InputStream in) throws IOException {
-					File file = new File(localDownloadDir + "/" + fileName);
-					FileUtils.writeByteArrayToFile(file, IOUtils.toByteArray(in));
-					files.add(file);
-				}
-			});
-		} catch (IOException ioException) {
-			throw new RuntimeException("IO Exception downloading files from Kartverket for dataSetId: " + dataSetId, ioException);
-		}
-		return files;
-	}
+                    FileUtils.writeByteArrayToFile(file, content);
+                    files.add(file);
+                }
+            });
+        } catch (IOException ioException) {
+            throw new RuntimeException("IO Exception downloading files from Kartverket for dataSetId: " + dataSetId, ioException);
+        }
+        return files;
+    }
 
-	Downloader getDownloader(String dataSetId, String format) {
-		Downloader kd;
-		if (isUUID(dataSetId)) {
-			// UUIDs are used in the API
-			kd = new GeoNorgeDownloadAPI();
-			if (format != null) {
-				kd.setFileNameFilter(n -> n.contains(format));
-			}
-		} else {
-			kd = new KartverketDownload(username, password);
-		}
-		return kd;
-	}
+    Downloader getDownloader(String dataSetId, String format) {
+        Downloader kd;
+        if (isUUID(dataSetId)) {
+            // UUIDs are used in the API
+            kd = new GeoNorgeDownloadAPI();
+            if (format != null) {
+                kd.setFileNameFilter(n -> n.contains(format));
+            }
+        } else {
+            kd = new KartverketDownload(username, password);
+        }
+        return kd;
+    }
 
-	private boolean isUUID(String dataSetId) {
-		try {
-			UUID.fromString(dataSetId);
-		} catch (IllegalArgumentException notUUIDException) {
-			return false;
-		}
-		return true;
-	}
+    private boolean isUUID(String dataSetId) {
+        try {
+            UUID.fromString(dataSetId);
+        } catch (IllegalArgumentException notUUIDException) {
+            return false;
+        }
+        return true;
+    }
 
 
 }
