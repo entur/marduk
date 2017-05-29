a1add11beafe027914bd990ccc7eef6b5fb4e840
erlendnils1 <erlendnils1@gmail.com>
2017-05-29 15:37:10 +0200

    NRP-1606 Use single route and single account to fetch files from sftp

--- src/main/java/no/rutebanken/marduk/routes/sftp/SftpReceiverRouteBuilder.java
+++ src/main/java/no/rutebanken/marduk/routes/sftp/SftpReceiverRouteBuilder.java
@@ -2,15 +2,18 @@ package no.rutebanken.marduk.routes.sftp;
 
 import no.rutebanken.marduk.Constants;
 import no.rutebanken.marduk.domain.Provider;
+import no.rutebanken.marduk.repository.ProviderRepository;
 import no.rutebanken.marduk.routes.BaseRouteBuilder;
 import no.rutebanken.marduk.routes.status.JobEvent;
-import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
+import java.util.Optional;
 import java.util.UUID;
 
 import static no.rutebanken.marduk.Constants.*;
@@ -24,61 +27,63 @@ public class SftpReceiverRouteBuilder extends BaseRouteBuilder {
     @Value("${sftp.host}")
     private String sftpHost;
 
+    @Value("${sftp.user:marduk}")
+    private String sftpUser;
+
     @Value("${sftp.keyfile}")
     private String sftpKeyFile;
 
     @Value("${sftp.known.hosts.file}")
     private String knownHostsFile;
 
+    @Value("${sftp.timetable.path:/incoming/timetable/}")
+    private String timetablePath;
+
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    @Autowired
+    private ProviderRepository providerRepository;
+
     @Override
     public void configure() throws Exception {
         super.configure();
 
-        CamelContext context = getContext();
+        singletonFrom("sftp://" + sftpUser + "@" + sftpHost + "?privateKeyFile=" + sftpKeyFile + "&knownHostsFile=" + knownHostsFile + "&recursive=true&sorter=#caseIdSftpSorter&delay={{sftp.delay}}&delete={{idempotent.skip:false}}&localWorkDirectory=files/tmp&connectTimeout=1000")
+                .autoStartup("{{sftp.autoStartup:true}}")
+                .filter(simple("${header." + Exchange.FILE_NAME + "} contains '" + timetablePath + "'"))
+                .process(e -> setProviderHeadersFromFileName(e))
+                .filter(simple("${header." + PROVIDER_ID + "} != null"))
+                .setHeader(Constants.FILE_NAME, header(Exchange.FILE_NAME))
+                .process(e -> e.getIn().setHeader(CORRELATION_ID, UUID.randomUUID().toString()))
+                .to("direct:filterDuplicateFile")
+                .log(LoggingLevel.INFO, correlation() + "Received new file on sftp route. Storing file ${header." + Exchange.FILE_NAME + "}'")
 
-        //TODO Catch changes in sftp account, restart route with new config?
-        Collection<Provider> providers = getProviderRepository().getProviders();
-        providers.stream().filter(p -> p.sftpAccount != null).forEach(p -> {
-            try {
-                context.addRoutes(new DynamcSftpPollerRouteBuilder(p, sftpHost, sftpKeyFile, knownHostsFile));
-            } catch (Exception e){
-                throw new RuntimeException(e);
-            }
-        });
+                .setHeader(FILE_HANDLE, simple(Constants.BLOBSTORE_PATH_INBOUND + "${header." + CHOUETTE_REFERENTIAL + "}/${header." + CHOUETTE_REFERENTIAL + "}-${date:now:yyyyMMddHHmmss}-${header.CamelFileNameOnly}"))
+                .log(LoggingLevel.INFO, correlation() + "File handle is: ${header." + FILE_HANDLE + "}")
+                .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
+                .to("direct:uploadBlob")
+                .process(e -> JobEvent.providerJobBuilder(e).timetableAction(JobEvent.TimetableAction.FILE_TRANSFER).state(JobEvent.State.STARTED).build())
+                .to("direct:updateStatus")
+                .log(LoggingLevel.INFO, correlation() + "Putting handle ${header." + FILE_HANDLE + "} on queue...")
+                .to("activemq:queue:ProcessFileQueue")
+                .routeId("sftp-gcs-route");
+
 
     }
 
-    private static final class DynamcSftpPollerRouteBuilder extends BaseRouteBuilder {
-        private final Provider provider;
-        private final String sftpHost;
-        private final String sftpKeyFile;
-        private final String knownHostsFile;
+    private void setProviderHeadersFromFileName(Exchange e) {
+        String fileName = e.getIn().getHeader(Exchange.FILE_NAME, String.class);
+        String providerSftpAccount = fileName.split(timetablePath)[0];
 
-        private DynamcSftpPollerRouteBuilder(Provider provider, String sftpHost, String sftpKeyFile, String knownHostsFile) {
-            this.provider = provider;
-            this.sftpHost = sftpHost;
-            this.sftpKeyFile = sftpKeyFile;
-            this.knownHostsFile = knownHostsFile;
-        }
+        Provider provider = providerRepository.getProviders().stream().filter(candidate -> providerSftpAccount.equals(candidate.sftpAccount)).findFirst().get();
 
-        @Override
-        public void configure() throws Exception {
-            singletonFrom("sftp://" + provider.sftpAccount + "@" + sftpHost + "?privateKeyFile=" + sftpKeyFile + "&knownHostsFile=" + knownHostsFile + "&sorter=#caseIdSftpSorter&delay={{sftp.delay}}&delete={{idempotent.skip:false}}&localWorkDirectory=files/tmp&connectTimeout=1000")
-					.autoStartup("{{sftp.autoStartup:true}}")
-                    .setHeader(PROVIDER_ID, constant(provider.id))
-                    .setHeader(Constants.FILE_NAME, header(Exchange.FILE_NAME))
-                    .process(e -> e.getIn().setHeader(CORRELATION_ID, UUID.randomUUID().toString()))
-                    .to("direct:filterDuplicateFile")
-                    .log(LoggingLevel.INFO, correlation()+"Received new file on sftp route for '" + provider.sftpAccount + "'. Storing file ...")
-                    .setHeader(FILE_HANDLE, simple(Constants.BLOBSTORE_PATH_INBOUND + provider.chouetteInfo.referential + "/" + provider.chouetteInfo.referential + "-${date:now:yyyyMMddHHmmss}-${header.CamelFileNameOnly}"))
-                    .log(LoggingLevel.INFO, correlation()+"File handle is: ${header." + FILE_HANDLE + "}")
-                    .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
-                    .to("direct:uploadBlob")
-                    .process(e -> JobEvent.providerJobBuilder(e).timetableAction(JobEvent.TimetableAction.FILE_TRANSFER).state(JobEvent.State.STARTED).build())
-                    .to("direct:updateStatus")
-                    .log(LoggingLevel.INFO, correlation()+"Putting handle ${header." + FILE_HANDLE + "} on queue...")
-                    .to("activemq:queue:ProcessFileQueue")
-                    .routeId("sftp-gcs-"+provider.chouetteInfo.referential);
+        if (provider != null) {
+            e.getIn().setHeader(PROVIDER_ID, provider.getId());
+            e.getIn().setHeader(CHOUETTE_REFERENTIAL, provider.getChouetteInfo().referential);
+        } else {
+            logger.warn("Found timetable file for unknown provider: " + fileName);
         }
     }
 
