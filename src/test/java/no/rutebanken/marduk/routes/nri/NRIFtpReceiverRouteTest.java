c35c5b0d08d07ac5e0b77e158983310deb60d8a2
erlendnils1 <erlendnils1@gmail.com>
2017-05-30 11:09:14 +0200

    NRP-1623 Added filtering by date for nri ftp file fetching

--- src/test/java/no/rutebanken/marduk/routes/nri/NRIFtpReceiverRouteTest.java
+++ src/test/java/no/rutebanken/marduk/routes/nri/NRIFtpReceiverRouteTest.java
@@ -12,6 +12,7 @@ import org.apache.camel.model.ModelCamelContext;
 import org.apache.camel.test.spring.CamelSpringRunner;
 import org.apache.camel.test.spring.UseAdviceWith;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,6 +30,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -36,89 +38,100 @@ import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.when;
 
 @RunWith(CamelSpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,classes = NRIFtpReceiverRouteBuilder.class, properties = "spring.main.sources=no.rutebanken.marduk.test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = NRIFtpReceiverRouteBuilder.class, properties = "spring.main.sources=no.rutebanken.marduk.test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-@ActiveProfiles({ "default", "in-memory-blobstore" })
+@ActiveProfiles({"default", "in-memory-blobstore"})
 @UseAdviceWith
 public class NRIFtpReceiverRouteTest extends MardukRouteBuilderIntegrationTestBase {
 
-	@Autowired
-	private ModelCamelContext context;
+    @Value("${nri.ftp.file.age.filter.months:3}")
+    private int fileAgeFilterMonths;
 
-	@Autowired
-	IdempotentRepositoryService idempotentRepositoryService;
+    @Autowired
+    private ModelCamelContext context;
 
-	@EndpointInject(uri = "mock:buskerud")
-	protected MockEndpoint buskerud;
+    @Autowired
+    IdempotentRepositoryService idempotentRepositoryService;
 
-	@Before
-	public void setUpProvider() throws IOException {
-		//wipe idempotent stores
-		idempotentRepositoryService.cleanUniqueFileNameAndDigestRepo();
-		when(providerRepository.getProvider(5L)).thenReturn(Provider.create(IOUtils.toString(new FileReader(
-				"src/test/resources/no/rutebanken/marduk/providerRepository/provider2.json"))));
-	}
+    @EndpointInject(uri = "mock:buskerud")
+    protected MockEndpoint buskerud;
 
-	@Test
-	public void testFetchFilesFromFTP() throws Exception {
+    @Before
+    public void setUpProvider() throws IOException {
+        //wipe idempotent stores
+        idempotentRepositoryService.cleanUniqueFileNameAndDigestRepo();
+        when(providerRepository.getProvider(5L)).thenReturn(Provider.create(IOUtils.toString(new FileReader(
+                                                                                                                   "src/test/resources/no/rutebanken/marduk/providerRepository/provider2.json"))));
+    }
 
-		FakeFtpServer fakeFtpServer = new FakeFtpServer();
-		fakeFtpServer.setSystemName("UNIX");
+    @Test
+    public void testFetchFilesFromFTP() throws Exception {
 
-		fakeFtpServer.addUserAccount(new UserAccount("username", "password", "/"));
+        FakeFtpServer fakeFtpServer = new FakeFtpServer();
+        fakeFtpServer.setSystemName("UNIX");
 
-		FileSystem fileSystem = new UnixFakeFileSystem();
-		fileSystem.add(new DirectoryEntry("/rutedata"));
-		fileSystem.add(new DirectoryEntry("/rutedata/Brakar (Buskerud fylke)"));
-		fileSystem.add(new DirectoryEntry("/rutedata/Brakar (Buskerud fylke)/1585"));
-		fileSystem.add(new DirectoryEntry("/rutedata/Brakar (Buskerud fylke)/1585/Hovedsett 2016_unzipped"));
-		fileSystem.add(new DirectoryEntry("/rutedata/Brakar (Buskerud fylke)/985"));
-		fileSystem.add(new DirectoryEntry("/rutedata/Brakar (Buskerud fylke)/984"));
-		fileSystem.add(new DirectoryEntry("/rutedata/Brakar (Buskerud fylke)/985/Hovedsett 2016_unzipped"));
-		FileEntry file1585 = new FileEntry("/rutedata/Brakar (Buskerud fylke)/1585/Hovedsett 2017.zip");
-		file1585.setContents(new byte[]{12,11,10});//IOUtils.toByteArray(getClass().getResourceAsStream("/no/rutebanken/marduk/routes/chouette/empty_regtopp.zip")));
-		fileSystem.add(file1585);
-		FileEntry file984 = new FileEntry("/rutedata/Brakar (Buskerud fylke)/984/Hovedsett 2016.zip");
-		file984.setContents(new byte[]{11,10,12});//IOUtils.toByteArray(getClass().getResourceAsStream("/no/rutebanken/marduk/routes/chouette/empty_regtopp.zip")));
-		fileSystem.add(file984 );
-		FileEntry file985_1 = new FileEntry("/rutedata/Brakar (Buskerud fylke)/985/Hovedsett 2016_v2.zip");
-		file985_1.setContents(new byte[]{10,11,12});//IOUtils.toByteArray(getClass().getResourceAsStream("/no/rutebanken/marduk/routes/chouette/empty_regtopp.zip")));
-		fileSystem.add(file985_1);
-		//idempotent filter should take care of this, since content is the same as above
-		FileEntry file985_2 = new FileEntry("/rutedata/Brakar (Buskerud fylke)/985/ShouldBeFiltered.zip");
-		file985_2.setContents(new byte[]{10,11,12});//IOUtils.toByteArray(getClass().getResourceAsStream("/no/rutebanken/marduk/routes/chouette/empty_regtopp.zip")));
-		fileSystem.add(file985_2);
+        fakeFtpServer.addUserAccount(new UserAccount("username", "password", "/"));
 
-		fakeFtpServer.setFileSystem(fileSystem);
-		fakeFtpServer.setServerControlPort(32220);
+        FileSystem fileSystem = new UnixFakeFileSystem();
+        fileSystem.add(new DirectoryEntry("/rutedata"));
+        fileSystem.add(new DirectoryEntry("/rutedata/Brakar (Buskerud fylke)"));
 
-		fakeFtpServer.start();
+        fileSystem.add(new DirectoryEntry("/rutedata/Brakar (Buskerud fylke)/1585"));
+        fileSystem.add(new DirectoryEntry("/rutedata/Brakar (Buskerud fylke)/1585/Hovedsett 2016_unzipped"));
+        fileSystem.add(new DirectoryEntry("/rutedata/Brakar (Buskerud fylke)/985"));
+        fileSystem.add(new DirectoryEntry("/rutedata/Brakar (Buskerud fylke)/984"));
+        fileSystem.add(new DirectoryEntry("/rutedata/Brakar (Buskerud fylke)/985/Hovedsett 2016_unzipped"));
+        FileEntry file1585 = new FileEntry("/rutedata/Brakar (Buskerud fylke)/1585/Hovedsett 2017.zip");
+        file1585.setLastModified(DateUtils.addMonths(new Date(), -(fileAgeFilterMonths - 1)));
+        file1585.setContents(new byte[]{12, 11, 10});//IOUtils.toByteArray(getClass().getResourceAsStream("/no/rutebanken/marduk/routes/chouette/empty_regtopp.zip")));
+        fileSystem.add(file1585);
+        FileEntry file984 = new FileEntry("/rutedata/Brakar (Buskerud fylke)/984/Hovedsett 2016.zip");
+        file984.setContents(new byte[]{11, 10, 12});//IOUtils.toByteArray(getClass().getResourceAsStream("/no/rutebanken/marduk/routes/chouette/empty_regtopp.zip")));
+        fileSystem.add(file984);
+        FileEntry file985_1 = new FileEntry("/rutedata/Brakar (Buskerud fylke)/985/Hovedsett 2016_v2.zip");
+        file985_1.setContents(new byte[]{10, 11, 12});//IOUtils.toByteArray(getClass().getResourceAsStream("/no/rutebanken/marduk/routes/chouette/empty_regtopp.zip")));
+        fileSystem.add(file985_1);
+        //idempotent filter should take care of this, since content is the same as above
+        FileEntry file985_2 = new FileEntry("/rutedata/Brakar (Buskerud fylke)/985/ShouldBeFiltered.zip");
+        file985_2.setContents(new byte[]{10, 11, 12});//IOUtils.toByteArray(getClass().getResourceAsStream("/no/rutebanken/marduk/routes/chouette/empty_regtopp.zip")));
+        fileSystem.add(file985_2);
 
-		context.getRouteDefinitions().get(0).adviceWith(context, new AdviceWithRouteBuilder() {
-			@Override
-			public void configure() throws Exception {
-				interceptSendToEndpoint("activemq:queue:ProcessFileQueue").skipSendToOriginalEndpoint()
-				.to("mock:buskerud");
-			}
-		});
-
-		// we must manually start when we are done with all the advice with
-		context.startRoute("nri-ftp-activemq");
-		context.start();
+        FileEntry toOld = new FileEntry("/rutedata/Brakar (Buskerud fylke)/985/TooOldShouldBeFiltered.zip");
+        toOld.setContents(new byte[]{10, 11, 12, 13});
+        toOld.setLastModified(DateUtils.addMonths(new Date(), -(fileAgeFilterMonths + 1)));
+        fileSystem.add(toOld);
 
 
-		// setup expectations on the mocks
-		buskerud.expectedMessageCount(3);
+        fakeFtpServer.setFileSystem(fileSystem);
+        fakeFtpServer.setServerControlPort(32220);
 
-		// assert that the test was okay
-		buskerud.assertIsSatisfied();
+        fakeFtpServer.start();
 
-		List<Exchange> exchanges = buskerud.getExchanges();
-		assertEquals(2L, exchanges.get(0).getIn().getHeader(Constants.PROVIDER_ID));
-		assertNotNull(exchanges.get(0).getIn().getHeader(Constants.CORRELATION_ID));
-		assertEquals("984_Hovedsett_2016.zip", exchanges.get(0).getIn().getHeader(Constants.FILE_NAME));
-		assertEquals("985_Hovedsett_2016_v2.zip", exchanges.get(1).getIn().getHeader(Constants.FILE_NAME));
-		assertEquals("1585_Hovedsett_2017.zip", exchanges.get(2).getIn().getHeader(Constants.FILE_NAME));
-	}
+        context.getRouteDefinitions().get(0).adviceWith(context, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                interceptSendToEndpoint("activemq:queue:ProcessFileQueue").skipSendToOriginalEndpoint()
+                        .to("mock:buskerud");
+            }
+        });
+
+        // we must manually start when we are done with all the advice with
+        context.startRoute("nri-ftp-activemq");
+        context.start();
+
+
+        // setup expectations on the mocks
+        buskerud.expectedMessageCount(3);
+
+        // assert that the test was okay
+        buskerud.assertIsSatisfied();
+
+        List<Exchange> exchanges = buskerud.getExchanges();
+        assertEquals(2L, exchanges.get(0).getIn().getHeader(Constants.PROVIDER_ID));
+        assertNotNull(exchanges.get(0).getIn().getHeader(Constants.CORRELATION_ID));
+        assertEquals("984_Hovedsett_2016.zip", exchanges.get(0).getIn().getHeader(Constants.FILE_NAME));
+        assertEquals("985_Hovedsett_2016_v2.zip", exchanges.get(1).getIn().getHeader(Constants.FILE_NAME));
+        assertEquals("1585_Hovedsett_2017.zip", exchanges.get(2).getIn().getHeader(Constants.FILE_NAME));
+    }
 
 }
