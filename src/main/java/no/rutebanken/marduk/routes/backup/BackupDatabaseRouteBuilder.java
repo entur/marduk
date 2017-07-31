2be10ebb48fca4a75c28ce66c5c61ff9ae41b743
Erlend Nossum <en@scienta.no>
2017-07-31 15:21:25 +0200

    NRP-1898 Corrected copy and paste error

--- src/main/java/no/rutebanken/marduk/routes/backup/BackupDatabaseRouteBuilder.java
+++ src/main/java/no/rutebanken/marduk/routes/backup/BackupDatabaseRouteBuilder.java
@@ -23,7 +23,7 @@ public class BackupDatabaseRouteBuilder extends BaseRouteBuilder {
     @Value("${database.backup.job:database-backup-pod.yaml}")
     private String databaseBackupFilename;
 
-    @Value("${fetch.osm.cron.schedule:0+*+3+?+*+*}")
+    @Value("${database.backup.cron.schedule:0+15+10+?+*+*}")
     private String cronSchedule;
 
     @Override
