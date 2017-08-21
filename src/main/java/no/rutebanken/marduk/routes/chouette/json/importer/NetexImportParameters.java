571b79e73e43eddf018dfe11d6c0c3aa79f9dde4
Arne Seime <arne@seimeconsulting.com>
2017-08-21 18:26:51 +0200

    Added Netex import parameters. Do not parse site frames. Do continue on line errors

--- src/main/java/no/rutebanken/marduk/routes/chouette/json/importer/NetexImportParameters.java
+++ src/main/java/no/rutebanken/marduk/routes/chouette/json/importer/NetexImportParameters.java
@@ -1,6 +1,8 @@
 package no.rutebanken.marduk.routes.chouette.json.importer;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import no.rutebanken.marduk.routes.chouette.json.ChouetteJobParameters;
 
 public class NetexImportParameters extends ChouetteJobParameters {
@@ -12,7 +14,23 @@ public class NetexImportParameters extends ChouetteJobParameters {
         public Netex netexImport;
     }
 
-    static class Netex extends AbstractImportParameters {}
+    static class Netex extends AbstractImportParameters {
+        @JsonProperty("parse_site_frames")
+        @JsonInclude(JsonInclude.Include.ALWAYS)
+    	private boolean parseSiteFrames = false;
+
+        @JsonProperty("validate_against_schema")
+        @JsonInclude(JsonInclude.Include.ALWAYS)
+    	private boolean validateAgainstSchema = true;
+
+        @JsonProperty("validate_against_profile")
+        @JsonInclude(JsonInclude.Include.ALWAYS)
+    	private boolean validateAgainstProfile = true;
+
+        @JsonProperty("continue_on_line_errors")
+        @JsonInclude(JsonInclude.Include.ALWAYS)
+    	private boolean continueOnLineErrors = true;
+    }
 
     public static NetexImportParameters create(String name, String referentialName, String organisationName, String userName, boolean cleanRepository, boolean enableValidation, boolean allowCreateMissingStopPlace, boolean enableStopPlaceIdMapping) {
         Netex netexImport = new Netex();
