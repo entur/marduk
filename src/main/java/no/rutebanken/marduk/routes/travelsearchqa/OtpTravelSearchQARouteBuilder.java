331372e7b4f71970392122b0986966f8938e2d6e
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-10-10 15:54:57 +0200

    NRP-2261 Update cron for travel search

--- src/main/java/no/rutebanken/marduk/routes/travelsearchqa/OtpTravelSearchQARouteBuilder.java
+++ src/main/java/no/rutebanken/marduk/routes/travelsearchqa/OtpTravelSearchQARouteBuilder.java
@@ -23,7 +23,7 @@ public class OtpTravelSearchQARouteBuilder extends BaseRouteBuilder {
     @Value("${otp.travelsearch.qa.job:otp-travelsearch-qa-pod.yaml}")
     private String otpTravelSearchJobFilename;
 
-    @Value("${otp.travelsearch.qa.cron.schedule:0+15+10+?+*+*}")
+    @Value("${otp.travelsearch.qa.cron.schedule:0+0/15+*+?+*+*}")
     private String cronSchedule;
 
     @Override
