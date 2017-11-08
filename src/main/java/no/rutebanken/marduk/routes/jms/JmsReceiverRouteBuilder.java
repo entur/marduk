8b22de490f017010323f5bd3ca19acf0fee7fc78
erlendnils1 <erlendnils1@gmail.com>
2017-11-08 12:42:08 +0100

    NRP-2357 Allow duplicate files when uploading netex from bel/ninkasi

--- src/main/java/no/rutebanken/marduk/routes/jms/JmsReceiverRouteBuilder.java
+++ src/main/java/no/rutebanken/marduk/routes/jms/JmsReceiverRouteBuilder.java
@@ -2,6 +2,8 @@ package no.rutebanken.marduk.routes.jms;
 
 import no.rutebanken.marduk.Constants;
 import no.rutebanken.marduk.routes.BaseRouteBuilder;
+import no.rutebanken.marduk.routes.file.FileType;
+import no.rutebanken.marduk.routes.file.beans.FileTypeClassifierBean;
 import no.rutebanken.marduk.routes.status.JobEvent;
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
@@ -20,24 +22,32 @@ public class JmsReceiverRouteBuilder extends BaseRouteBuilder {
         super.configure();
 
         from("activemq:queue:MardukInboundQueue?transacted=true").streamCaching()
-            .transacted()
-            .setHeader(Exchange.FILE_NAME, header(Constants.FILE_NAME))
-            .log(LoggingLevel.INFO, correlation()+"Received notification about file '${header." + Constants.FILE_NAME + "}' on jms. Fetching file ...")
-            .log(LoggingLevel.INFO, correlation() + "Fetching blob ${header." + FILE_HANDLE + "}")
-            .to("direct:fetchExternalBlob")
-            .process(e -> e.getIn().setHeader(CHOUETTE_REFERENTIAL, getProviderRepository().getProvider(e.getIn().getHeader(PROVIDER_ID, Long.class)).chouetteInfo.referential))
-            .to("direct:filterDuplicateFile")
-            .log(LoggingLevel.INFO, correlation() + "File handle is: ${header." + FILE_HANDLE + "}")
-            .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
-            .to("direct:uploadBlob")
-            .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
-            .process(e -> JobEvent.providerJobBuilder(e).timetableAction(JobEvent.TimetableAction.FILE_TRANSFER).state(JobEvent.State.STARTED).build())
-            .to("direct:updateStatus")
-            .choice()
+                .transacted()
+                .setHeader(Exchange.FILE_NAME, header(Constants.FILE_NAME))
+                .log(LoggingLevel.INFO, correlation() + "Received notification about file '${header." + Constants.FILE_NAME + "}' on jms. Fetching file ...")
+                .log(LoggingLevel.INFO, correlation() + "Fetching blob ${header." + FILE_HANDLE + "}")
+                .to("direct:fetchExternalBlob")
+                .process(e -> e.getIn().setHeader(CHOUETTE_REFERENTIAL, getProviderRepository().getProvider(e.getIn().getHeader(PROVIDER_ID, Long.class)).chouetteInfo.referential))
+                .convertBodyTo(byte[].class)
+                .validate().method(FileTypeClassifierBean.class, "validateFile")
+                .choice()
+                .when(header(FILE_TYPE).isEqualTo(FileType.NETEXPROFILE.name()))
+                    .log(LoggingLevel.INFO, correlation() + "Ignoring duplicate filter for netexprofile file: ${header." + FILE_HANDLE + "}")
+                .otherwise()
+                    .to("direct:filterDuplicateFile")
+                .end()
+
+                .log(LoggingLevel.INFO, correlation() + "File handle is: ${header." + FILE_HANDLE + "}")
+                .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
+                .to("direct:uploadBlob")
+                .to("log:" + getClass().getName() + "?level=DEBUG&showAll=true&multiline=true")
+                .process(e -> JobEvent.providerJobBuilder(e).timetableAction(JobEvent.TimetableAction.FILE_TRANSFER).state(JobEvent.State.STARTED).build())
+                .to("direct:updateStatus")
+                .choice()
                 .when(simple("{{blobstore.delete.external.blobs:true}}"))
                 .to("direct:deleteExternalBlob")
-             .end()
-            .to("activemq:queue:ProcessFileQueue");
+                .end()
+                .to("activemq:queue:ProcessFileQueue");
 
     }
 
