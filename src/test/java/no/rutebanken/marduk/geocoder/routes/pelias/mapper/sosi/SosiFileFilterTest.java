bdbb8f2e4a23d6c492ebf6d36b27d0ee39aa5f46
erlendnils1 <erlendnils1@gmail.com>
2018-01-12 09:21:24 +0100

    ROR-64 Removed tmp debug logging

--- src/test/java/no/rutebanken/marduk/geocoder/routes/pelias/mapper/sosi/SosiFileFilterTest.java
+++ src/test/java/no/rutebanken/marduk/geocoder/routes/pelias/mapper/sosi/SosiFileFilterTest.java
@@ -7,11 +7,8 @@ import no.rutebanken.marduk.geocoder.sosi.SosiElementWrapperFactory;
 import no.rutebanken.marduk.geocoder.sosi.SosiFileFilter;
 import org.junit.Assert;
 import org.junit.Test;
-import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Collection;
 
 public class SosiFileFilterTest {
@@ -20,8 +17,6 @@ public class SosiFileFilterTest {
     public void filterByType() throws Exception {
         String targetFile = "target/filtered.sos";
         new SosiFileFilter().filterElements(new FileInputStream("src/test/resources/no/rutebanken/marduk/geocoder/sosi/placeNames.sos"), targetFile, kv -> "NAVNEOBJEKTTYPE".equals(kv.getKey()) && "industriområde".equals(kv.getValue()));
-        LoggerFactory.getLogger(getClass()).warn("TODO Filtered content:" + new String(Files.readAllBytes(Paths.get(targetFile))));
-
 
         Collection<ElasticsearchCommand> commands = new KartverketSosiStreamToElasticsearchCommands(new SosiElementWrapperFactory(), 1).transform(new FileInputStream(targetFile));
 
