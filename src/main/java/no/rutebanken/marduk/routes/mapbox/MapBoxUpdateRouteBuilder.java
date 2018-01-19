1680c6922b246178632b35f8f84037db8b276903
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-01-19 08:56:01 +0100

    ROR-84 Cleanup mapbox route builder. Fix retry poll logic

--- src/main/java/no/rutebanken/marduk/routes/mapbox/MapBoxUpdateRouteBuilder.java
+++ src/main/java/no/rutebanken/marduk/routes/mapbox/MapBoxUpdateRouteBuilder.java
@@ -1,10 +1,6 @@
 package no.rutebanken.marduk.routes.mapbox;
 
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicSessionCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import no.rutebanken.marduk.geocoder.routes.tiamat.TiamatGeoCoderExportRouteBuilder;
 import no.rutebanken.marduk.routes.BaseRouteBuilder;
 import no.rutebanken.marduk.routes.file.ZipFileUtils;
@@ -22,18 +18,15 @@ import java.io.File;
 import java.io.FileInputStream;
 
 import static no.rutebanken.marduk.Constants.FILE_HANDLE;
+import static no.rutebanken.marduk.Constants.LOOP_COUNTER;
 
 @Component
 public class MapBoxUpdateRouteBuilder extends BaseRouteBuilder {
 
-    private static final String FILE_EXTENSION = "RutebankenFileExtension";
-    private static final String CONVERSION_ROUTE = "RutebankenConversionRoute";
-    private static final String WORKING_DIRECTORY = "RutebankenWorkingDirectory";
     private static final String TIAMAT_EXPORT_GCP_PATH = "tiamat-export";
+    private static final int POLL_MAPBOX_UPLOAD_STATUS_DELAY = 15000;
+    private static final String TIAMAT_GEOSJON_FILENAME = "tiamat.geojson";
 
-    /**
-     * One time per 24H on MON-FRI
-     */
     @Value("${mapbox.update.cron.schedule:0+0+23+?+*+MON-FRI}")
     private String cronSchedule;
 
@@ -59,28 +52,37 @@ public class MapBoxUpdateRouteBuilder extends BaseRouteBuilder {
     @Value("${mapbox.aws.region:us-east-1")
     private String awsRegion;
 
+    @Value("${mapbox.upload.status.max.retries:10}")
+    private int maxRetries;
+
     @Override
     public void configure() throws Exception {
         super.configure();
 
-
-        singletonFrom("quartz2://marduk/mapBoxUpdate?cron=" + cronSchedule + "&trigger.timeZone=Europe/Oslo")
+        singletonFrom("quartz2://marduk/mapboxUpdate?cron=" + cronSchedule + "&trigger.timeZone=Europe/Oslo")
                 .autoStartup("{{mapbox.update.autoStartup:false}}")
                 .filter(e -> isLeader(e.getFromRouteId()))
-                .log(LoggingLevel.INFO, "Update mapbox tileset")
-                .to("direct:downloadUnzipMapboxData")
+                .log(LoggingLevel.INFO, "Update mapbox tileset from tiamat export started by cron")
+                .to("direct:uploadTiamatToMapboxAsGeoJson")
                 .routeId("mapbox-update-quartz");
 
         from("direct:uploadTiamatToMapboxAsGeoJson")
                 .setHeader(TIAMAT_EXPORT_GCP_PATH, simple(blobStoreSubdirectoryForTiamatGeoCoderExport + "/" + TiamatGeoCoderExportRouteBuilder.TIAMAT_EXPORT_LATEST_FILE_NAME))
                 .to("direct:recreateLocalMapboxDirectory")
-                .to("direct:downloadLatestTiamatExportToFolder")
+                .to("direct:downloadLatestTiamatExportToMapboxFolder")
                 .to("direct:mapboxUnzipLatestTiamatExportToFolder")
                 .to("direct:retrieveMapboxAwsCredentials")
-                .process(e -> e.getIn().setBody( FileUtils.listFiles(new File(localWorkingDirectory + "/tiamat"), new String[]{"xml"}, true).stream().findFirst().get()))
+                .to("direct:findFirstXmlFileRecursive")
                 .to("direct:transformToGeoJsonFromTiamat")
-                .setHeader("filename", constant("tiamat.geojson"))
-                .bean("awsS3Uploader", "upload")
+                .to("direct:uploadMapboxDataAws")
+                .to("direct:initiateMapboxUpload")
+                .delay(POLL_MAPBOX_UPLOAD_STATUS_DELAY)
+                .to("direct:pollRetryMapboxStatus")
+                .to("direct:recreateLocalMapboxDirectory")
+                .log(LoggingLevel.INFO, "Finished inserting tiamat data")
+                .routeId("mapbox-convert-tiamat-data");
+
+        from("direct:initiateMapboxUpload")
                 .process(exchange -> exchange.getOut().setBody(new MapboxUploadRequest(mapboxUser  + ".automated-uploaded-tileset", ((MapBoxAwsCredentials) exchange.getIn().getHeader("credentials")).getUrl(), exchange.getIn().getHeader("filename").toString())))
                 .marshal().json(JsonLibrary.Jackson)
                 .log(LoggingLevel.INFO, "${body}")
@@ -88,40 +90,46 @@ public class MapBoxUpdateRouteBuilder extends BaseRouteBuilder {
                 .to(mapboxApiUrl + "/uploads/v1/" + mapboxUser + "?access_token=" + mapboxAccessToken)
                 .unmarshal().json(JsonLibrary.Jackson, MapBoxUploadStatus.class)
                 .log(LoggingLevel.INFO, "Received ${body}")
-                .to("direct:pollMapboxStatus")
-                .log(LoggingLevel.INFO, "Finished inserting tiamat data")
-                .routeId("mapbox-convert-tiamat-data");
+                .routeId("initiate-mapbox-upload");
 
-        from("direct:pollMapboxStatus")
-                .loop(10)
+        from("direct:uploadMapboxDataAws")
+                .setHeader("filename", constant(TIAMAT_GEOSJON_FILENAME))
+                .bean("awsS3Uploader", "upload")
+                .routeId("upload-mapbox-data-aws");
 
-                .choice()
-                .when(simple("${body.error}"))
-                .to("direct:errorUploadMapboxTileset")
-                .end() // End choice
+        from("direct:findFirstXmlFileRecursive")
+                .process(e -> e.getIn().setBody( FileUtils.listFiles(new File(localWorkingDirectory + "/tiamat"), new String[]{"xml"}, true).stream().findFirst().get()))
+                .routeId("mapbox-find-first-xml-file-recursive");
+
+        from("direct:pollRetryMapboxStatus")
+                .loopDoWhile(simple("${body.error} == null && ${body.complete} == false"))
+                .process(e -> e.getIn().setHeader(LOOP_COUNTER, (Integer) e.getIn().getHeader(LOOP_COUNTER, 0) + 1))
+                .log("loop counter ${header." + LOOP_COUNTER + "}")
+                .to("direct:endIfMapboxUploadError")
 
                 .choice()
                     .when(simple("${body.complete}"))
-                    .log("complete ${body}")
-                    .to("direct:mapboxEnd")
+                        .log("complete ${body}")
+                        .to("direct:mapboxEnd")
                     .otherwise()
-                    .log(LoggingLevel.INFO, "${body.id} Not complete yet.. wait a bit and try again")
-                    .delay(5000)
-                    .to("direct:fetchMapboxUploadStatus")
-                    .end() // End choice
-                .end() // End loop
+                        .log(LoggingLevel.INFO, "${body.id} Not complete yet.. wait a bit and try again")
+                        .delay(POLL_MAPBOX_UPLOAD_STATUS_DELAY)
+                        .to("direct:fetchMapboxUploadStatus")
+                .endChoice()
 
                 .choice()
-                .when(simple("${body.complete}"))
-                .otherwise()
-                .log("Gave up looking for complete status after 10 attempts")
-                .end() // End choice
+                    .when(simple("${header." + LOOP_COUNTER + "} > " + maxRetries))
+                        .log(LoggingLevel.WARN, getClass().getName(), "Giving up after looping after " + maxRetries + " iterations")
+                        .stop() // end route?
+                .endChoice()
+                .routeId("mapbox-poll-retry-upload-status");
 
-                .routeId("mapbox-poll-upload-status");
-
-        from("direct:errorUploadMapboxTileset")
-                .log(LoggingLevel.ERROR, "Got error uploading tileset. ${body}")
-                .end();
+        from("direct:endIfMapboxUploadError")
+                .choice()
+                    .when(simple("${body.error}"))
+                    .log(LoggingLevel.ERROR, "Got error uploading tileset. ${body}")
+                    .stop()
+                .endChoice();
 
         from("direct:fetchMapboxUploadStatus")
                 .setProperty("tilesetId", simple("${body.id}"))
@@ -138,18 +146,15 @@ public class MapBoxUpdateRouteBuilder extends BaseRouteBuilder {
                 .end()
                 .routeId("mapbox-upload-complete-end");
 
-
-
         from("direct:retrieveMapboxAwsCredentials")
                 .setHeader(Exchange.HTTP_METHOD, constant(org.apache.camel.component.http4.HttpMethods.POST))
-                .to(mapboxApiUrl + "/uploads/v1/" + mapboxUser + "/credentials?access_token="+mapboxAccessToken)
+                .to(mapboxApiUrl + "/uploads/v1/" + mapboxUser + "/credentials?access_token=" + mapboxAccessToken)
                 .unmarshal().json(JsonLibrary.Jackson, MapBoxAwsCredentials.class)
                 .setHeader("credentials", simple("${body}"))
                 .log(LoggingLevel.INFO, "retrieved credentials: ${header.credentials}")
                 .routeId("mapbox-retrieve-aws-credentials");
 
-
-        from("direct:downloadLatestTiamatExportToFolder")
+        from("direct:downloadLatestTiamatExportToMapboxFolder")
                 .setHeader(FILE_HANDLE, header(TIAMAT_EXPORT_GCP_PATH))
                 .to("direct:getBlob")
                 .choice()
@@ -173,9 +178,6 @@ public class MapBoxUpdateRouteBuilder extends BaseRouteBuilder {
         from("direct:transformToGeoJsonFromTiamat")
                 .log(LoggingLevel.INFO, "convert tiamat data to geojson")
                 .bean("deliveryPublicationStreamToGeoJson", "transform")
-                .routeId("mapbox-convert-from-tiamat");
-
-
+                .routeId("mapbox-transform-from-tiamat");
     }
-
 }
