3ddcfde38d2180d7df69aa81126fad0da2d80348
vincent <vincent.paturet@entur.org>
2019-02-13 11:13:38 +0100

    ROR-595 trigger NeTex flexible transport and static lines merging when a new flexible transport export is available

---
+++ src/main/java/no/rutebanken/marduk/repository/LocalDiskBlobStoreRepository.java
@@ -0,0 +1,116 @@
+/*
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ *   https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+
+package no.rutebanken.marduk.repository;
+
+import com.google.cloud.storage.Storage;
+import no.rutebanken.marduk.domain.BlobStoreFiles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+
+/**
+ * Simple file-based blob store repository for testing purpose.
+ */
+@Component
+@Profile("local-disk-blobstore")
+public class LocalDiskBlobStoreRepository implements BlobStoreRepository {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Value("${blobstore.local.folder:files/blob}")
+    private String baseFolder;
+
+    @Override
+    public BlobStoreFiles listBlobs(Collection<String> prefixes) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public BlobStoreFiles listBlobs(String prefix) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public BlobStoreFiles listBlobsFlat(String prefix) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public InputStream getBlob(String objectName) {
+        logger.debug("get blob called in local-disk blob store");
+        Path path = Paths.get(baseFolder).resolve(objectName);
+        if (!path.toFile().exists()) {
+            return null;
+        }
+        try {
+            // converted as ByteArrayInputStream so that Camel stream cache can reopen it
+            // since ByteArrayInputStream.close() does nothing
+            return new ByteArrayInputStream(Files.readAllBytes(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void uploadBlob(String objectName, InputStream inputStream, boolean makePublic) {
+        logger.debug("upload blob called in local-disk blob store");
+        try {
+            Path localPath = Paths.get(objectName);
+
+            Path folder = Paths.get(baseFolder).resolve(localPath.getParent());
+            Files.createDirectories(folder);
+
+            Path fullPath = Paths.get(baseFolder).resolve(localPath);
+            Files.deleteIfExists(fullPath);
+
+            Files.copy(inputStream, fullPath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void uploadBlob(String objectName, InputStream inputStream, boolean makePublic, String contentType) {
+        uploadBlob(objectName, inputStream, makePublic);
+    }
+
+    @Override
+    public void setStorage(Storage storage) {
+    }
+
+    @Override
+    public void setContainerName(String containerName) {
+    }
+
+    @Override
+    public boolean delete(String objectName) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean deleteAllFilesInFolder(String folder) {
+        throw new UnsupportedOperationException();
+    }
+}
