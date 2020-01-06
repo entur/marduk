c7ba67013eac1c705d2b7664dcaf6876dad045dc
vincent <vincent.paturet@entur.org>
2020-01-06 16:16:47 +0100

    ROR-724 Remove OTP dependency from marduk

--- src/main/java/no/rutebanken/marduk/routes/otp/remote/OtpGraphBuilder.java
+++ src/main/java/no/rutebanken/marduk/routes/otp/remote/OtpGraphBuilder.java
@@ -3,7 +3,6 @@ package no.rutebanken.marduk.routes.otp.remote;
 /**
  * Build an OTP graph.
  * @see KubernetesJobGraphBuilder creates a Kubernetes job to build the graph in a dedicated pod.
- * @see InVMGraphBuilder builds the graph locally for testing purpose.
  */
 public interface OtpGraphBuilder {
 
