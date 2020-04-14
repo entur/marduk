3b9ceb33211ef08ac2a59453e489d3b805a8e39d
vincent <vincent.paturet@entur.org>
2020-04-14 10:33:50 +0200

    Improve GTFS export

--- src/main/java/no/rutebanken/marduk/gtfs/GtfsExport.java
+++ src/main/java/no/rutebanken/marduk/gtfs/GtfsExport.java
@@ -21,7 +21,7 @@ public enum GtfsExport {
             "calendar_dates.txt", "service_id,date,exception_type".split(","),
             "routes.txt", "agency_id,route_id,route_short_name,route_long_name,route_type,route_desc,route_url,route_color,route_text_color".split(","),
             "shapes.txt", "shape_id,shape_pt_sequence,shape_pt_lat,shape_pt_lon,shape_dist_traveled".split(","),
-            "stops.txt", "stop_id,stop_name,stop_lat,stop_lon,stop_desc,location_type,parent_station,wheelchair_boarding,vehicle_type,platform_code".split(","),
+            "stops.txt", "stop_id,stop_name,stop_lat,stop_lon,stop_desc,location_type,parent_station,wheelchair_boarding,stop_timezone,vehicle_type,platform_code".split(","),
             "stop_times.txt", "trip_id,stop_id,arrival_time,departure_time,stop_sequence,stop_headsign,pickup_type,drop_off_type,shape_dist_traveled".split(","),
             "transfers.txt", "from_stop_id,from_trip_id,to_stop_id,to_trip_id,transfer_type".split(","),
             "trips.txt", "route_id,trip_id,service_id,trip_headsign,direction_id,wheelchair_accessible".split(","))),
