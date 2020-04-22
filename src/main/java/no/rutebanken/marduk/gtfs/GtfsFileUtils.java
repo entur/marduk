6f5c1ecab311b00b1b7ba125c2f456aeca814754
vincent <vincent.paturet@entur.org>
2020-04-22 10:46:26 +0200

    Fixed transfers deduplication

--- src/main/java/no/rutebanken/marduk/gtfs/GtfsFileUtils.java
+++ src/main/java/no/rutebanken/marduk/gtfs/GtfsFileUtils.java
@@ -19,7 +19,6 @@ package no.rutebanken.marduk.gtfs;
 import no.rutebanken.marduk.exceptions.MardukException;
 import no.rutebanken.marduk.routes.file.TempFileUtils;
 import no.rutebanken.marduk.routes.file.ZipFileUtils;
-import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.FileSystemUtils;
@@ -33,8 +32,13 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GtfsFileUtils {
 
@@ -43,21 +47,38 @@ public class GtfsFileUtils {
     public static final String FEED_INFO_FILE_NAME = "feed_info.txt";
     private static final byte[] FEED_INFO_FILE_CONTENT = "feed_id,feed_publisher_name,feed_publisher_url,feed_lang\nENTUR,Entur,https://www.entur.org,no".getBytes(StandardCharsets.UTF_8);
 
+
+    /**
+     * Merge all GTFS files in a given directory.
+     * Files are merged in alphabetical order.
+     *
+     * @param sourceDirectory the directory containing the GTFS archives
+     * @param gtfsExport      the type of GTFS export
+     * @return a delete-on-close input stream refering to the resulting merged GTFS archive.
+     */
     public static InputStream mergeGtfsFilesInDirectory(String sourceDirectory, GtfsExport gtfsExport) {
 
-        Collection<File> zipFiles = FileUtils.listFiles(new File(sourceDirectory), new String[]{"zip"}, false);
-        try {
-            return TempFileUtils.createDeleteOnCloseInputStream(mergeGtfsFiles(zipFiles, gtfsExport));
+        try (Stream<Path> walk = Files.walk(Paths.get(sourceDirectory))) {
+            List<File> zipfilesInDirectory = walk.filter(Files::isRegularFile)
+                    .filter(p -> p.endsWith(".zip"))
+                    .sorted(Comparator.naturalOrder())
+                    .map(Path::toFile)
+                    .collect(Collectors.toList());
+
+            return TempFileUtils.createDeleteOnCloseInputStream(mergeGtfsFiles(zipfilesInDirectory, gtfsExport));
+
         } catch (IOException e) {
             throw new MardukException(e);
         }
 
+
     }
 
     /**
-     * merge GTFS file entries in a working directory, add the feed_info.txt entry and return the zipped content of
+     * merge a collection of GTFS file entries, add the feed_info.txt entry and return the zipped content of
      * the working directory
-     * @param zipFiles GTFS archives to be merged
+     *
+     * @param zipFiles   GTFS archives to be merged
      * @param gtfsExport the type of export
      * @return a zip file containing the merged GTFS data
      * @throws IOException
