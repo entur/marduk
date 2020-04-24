9fa5c5ef2bfc633dd0b0ad6e563ed728d7eb1c23
vincent <vincent.paturet@entur.org>
2020-04-24 14:55:11 +0200

    Refactor GTFS transformation

--- src/test/java/no/rutebanken/marduk/gtfs/GtfsTransformationServiceTest.java
+++ src/test/java/no/rutebanken/marduk/gtfs/GtfsTransformationServiceTest.java
@@ -16,24 +16,20 @@
 
 package no.rutebanken.marduk.gtfs;
 
-import no.rutebanken.marduk.gtfs.BasicRouteTypeCode;
-import no.rutebanken.marduk.gtfs.GtfsFileUtils;
-import no.rutebanken.marduk.gtfs.GtfsTransformationService;
 import no.rutebanken.marduk.routes.file.ZipFileUtils;
 import no.rutebanken.marduk.routes.google.GoogleRouteTypeCode;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -46,13 +42,14 @@ public class GtfsTransformationServiceTest {
 
     private File getExtendedGtfsTestFile() throws IOException {
         Path extendedGTFSFile = Files.createTempFile("extendedGTFSFile", ".zip");
-        Files.copy(Path.of( "src/test/resources/no/rutebanken/marduk/routes/gtfs/extended_gtfs.zip"), extendedGTFSFile, StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(Path.of("src/test/resources/no/rutebanken/marduk/routes/gtfs/extended_gtfs.zip"), extendedGTFSFile, StandardCopyOption.REPLACE_EXISTING);
         return extendedGTFSFile.toFile();
     }
 
     @Test
     public void transformToGoogleFormatExcludeShapes() throws Exception {
-        File target  = new GtfsTransformationService().transformToGoogleFormat(getExtendedGtfsTestFile(), false);
+
+        File target = GtfsFileUtils.mergeGtfsFiles(Collections.singleton(getExtendedGtfsTestFile()), GtfsExport.GTFS_GOOGLE, false);
 
         assertRouteRouteTypesAreConvertedToGoogleSupportedValues(target);
         assertStopVehicleTypesAreConvertedToGoogleSupportedValues(target);
@@ -62,7 +59,8 @@ public class GtfsTransformationServiceTest {
 
     @Test
     public void transformToGoogleFormatIncludeShapes() throws Exception {
-        File target  = new GtfsTransformationService().transformToGoogleFormat(getExtendedGtfsTestFile(), true);
+
+        File target = GtfsFileUtils.mergeGtfsFiles(Collections.singleton(getExtendedGtfsTestFile()), GtfsExport.GTFS_GOOGLE, true);
 
         assertRouteRouteTypesAreConvertedToGoogleSupportedValues(target);
         assertStopVehicleTypesAreConvertedToGoogleSupportedValues(target);
@@ -73,7 +71,9 @@ public class GtfsTransformationServiceTest {
 
     @Test
     public void transformToBasicGTFSFormatExcludeShapes() throws Exception {
-        File target  = new GtfsTransformationService().transformToBasicGTFSFormat(getExtendedGtfsTestFile(), false);
+
+        File target = GtfsFileUtils.mergeGtfsFiles(Collections.singleton(getExtendedGtfsTestFile()), GtfsExport.GTFS_BASIC, false);
+
 
         assertRouteRouteTypesAreConvertedToBasicGtfsValues(target);
         assertStopVehicleTypesAreConvertedToBasicGtfsValues(target);
@@ -83,7 +83,9 @@ public class GtfsTransformationServiceTest {
 
     @Test
     public void transformToBasicGTFSFormatIncludeShapes() throws Exception {
-        File target  = new GtfsTransformationService().transformToBasicGTFSFormat(getExtendedGtfsTestFile(), true);
+
+        File target = GtfsFileUtils.mergeGtfsFiles(Collections.singleton(getExtendedGtfsTestFile()), GtfsExport.GTFS_BASIC, true);
+
 
         assertRouteRouteTypesAreConvertedToBasicGtfsValues(target);
         assertStopVehicleTypesAreConvertedToBasicGtfsValues(target);
@@ -108,9 +110,9 @@ public class GtfsTransformationServiceTest {
         List<String> stopLines = IOUtils.readLines(new ByteArrayInputStream(ZipFileUtils.extractFileFromZipFile(out, "stops.txt")), StandardCharsets.UTF_8);
         stopLines.remove(0); // remove header
         assertThat(stopLines.get(0)).as("Line without vehicle type should not be changed").endsWith(",");
-        assertThat(stopLines.get(1)).as("Line with valid value 701 should be kept").endsWith(",701");
-        assertThat(stopLines.get(2)).as("Line with extended value 1012 should be converted to 1000").endsWith(",1000");
-        assertThat(stopLines.get(3)).as("Line with extended value 1601 should be converted to 1700 (default)").endsWith(",1700");
+        assertThat(stopLines.get(1)).as("Line with valid value 701 should be kept").endsWith(",701,");
+        assertThat(stopLines.get(2)).as("Line with extended value 1012 should be converted to 1000").endsWith(",1000,");
+        assertThat(stopLines.get(3)).as("Line with extended value 1601 should be converted to 1700 (default)").endsWith(",1700,");
     }
 
     public static void assertRouteRouteTypesAreConvertedToBasicGtfsValues(File out) throws IOException {
@@ -129,9 +131,9 @@ public class GtfsTransformationServiceTest {
     public static void assertStopVehicleTypesAreConvertedToBasicGtfsValues(File out) throws IOException {
         List<String> stopLines = IOUtils.readLines(new ByteArrayInputStream(ZipFileUtils.extractFileFromZipFile(out, "stops.txt")), StandardCharsets.UTF_8);
         stopLines.remove(0); // remove header
-        assertThat(stopLines.get(1)).as("Line with valid value 701 should be converted to 3").endsWith(",3");
-        assertThat(stopLines.get(2)).as("Line with extended value 1012 should be converted to 4").endsWith(",4");
-        assertThat(stopLines.get(3)).as("Line with extended value 1601 should be converted to 3 (default)").endsWith(",3");
+        assertThat(stopLines.get(1)).as("Line with valid value 701 should be converted to 3").endsWith(",3,");
+        assertThat(stopLines.get(2)).as("Line with extended value 1012 should be converted to 4").endsWith(",4,");
+        assertThat(stopLines.get(3)).as("Line with extended value 1601 should be converted to 3 (default)").endsWith(",3,");
         List<String> feedInfoLines = IOUtils.readLines(new ByteArrayInputStream(ZipFileUtils.extractFileFromZipFile(out, GtfsFileUtils.FEED_INFO_FILE_NAME)), StandardCharsets.UTF_8);
 
     }
