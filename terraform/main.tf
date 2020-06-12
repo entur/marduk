231b59a1f85c6b2cdad91c6ba6839ad93a7233f8
vincent <vincent.paturet@entur.org>
2020-06-12 11:50:30 +0200

    Refactored Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,12 +14,12 @@ provider "kubernetes" {
 resource "google_service_account" "marduk_service_account" {
   account_id = "${var.labels.team}-${var.labels.app}-sa"
   display_name = "${var.labels.team}-${var.labels.app} service account"
-  project = var.gcp_project
+  project = var.gcp_resources_project
 }
 
 # add service account as member to the cloudsql client
 resource "google_project_iam_member" "cloudsql_iam_member" {
-  project = var.gcp_project
+  project = var.gcp_cloudsql_project
   role = var.service_account_cloudsql_role
   member = "serviceAccount:${google_service_account.marduk_service_account.email}"
 }
@@ -48,14 +48,6 @@ resource "google_storage_bucket_iam_member" "storage_otpreport_bucket_iam_member
 # add service account as member to pubsub service in the resources project
 resource "google_project_iam_member" "pubsub_project_iam_member" {
   project = var.gcp_pubsub_project
-  role = var.service_account_pubsub_role
-  member = "serviceAccount:${google_service_account.marduk_service_account.email}"
-}
-
-# add service account as member to pubsub service in the workload project
-# TODO to be removed after cluster migration
-resource "google_project_iam_member" "pubsub_iam_member" {
-  project = var.gcp_project
   role = var.service_account_pubsub_role
   member = "serviceAccount:${google_service_account.marduk_service_account.email}"
 }
@@ -93,7 +85,8 @@ resource "kubernetes_secret" "ror-marduk-secret" {
   }
 }
 
-# Create database
+/** Deactivate resource creation until workloads are moved to the new cluster.
+
 module "postgres" {
   source = "github.com/entur/terraform//modules/postgres"
   postgresql_version = "POSTGRES_9_6"
@@ -358,3 +351,4 @@ resource "google_pubsub_subscription" "Otp2GraphBuildQueue" {
   project = var.gcp_pubsub_project
   labels = var.labels
 }
+**/
