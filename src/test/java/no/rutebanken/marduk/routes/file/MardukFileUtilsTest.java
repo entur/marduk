352bd14aae68d537dd6baf3f3bc6479be918e287
vincent <vincent.paturet@entur.org>
2020-09-16 12:06:59 +0200

    Code cleanup

--- src/test/java/no/rutebanken/marduk/routes/file/MardukFileUtilsTest.java
+++ src/test/java/no/rutebanken/marduk/routes/file/MardukFileUtilsTest.java
@@ -1,16 +1,15 @@
-package no.rutebanken.marduk.routes.file.beans;
+package no.rutebanken.marduk.routes.file;
 
-import no.rutebanken.marduk.routes.file.MardukFileUtils;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class MardukFileUtilsTest {
+class MardukFileUtilsTest {
 
     @Test
-    public void testIsValidFileName() {
+    void testIsValidFileName() {
         assertTrue(MardukFileUtils.isValidFileName("test.xml"), "A Filename without special character is valid");
         assertTrue(MardukFileUtils.isValidFileName("ÅØÆåøæ.xml"), "A Filename with ISO-8859-1 characters is valid");
         assertFalse(MardukFileUtils.isValidFileName("谢谢.xml"), "A Filename with non ISO-8859-1 characters is invalid");
@@ -21,13 +20,12 @@ public class MardukFileUtilsTest {
     }
 
     @Test
-    public void testIsSanitizeFileName() {
+    void testSanitizeFileName() {
         assertEquals("test.xml", MardukFileUtils.sanitizeFileName("test.xml"), "A Filename without special character is unchanged");
         assertEquals("ÅØÆåøæ.xml", MardukFileUtils.sanitizeFileName("ÅØÆåøæ.xml"), "A Filename with ISO-8859-1 characters is unchanged");
         assertEquals(".xml", MardukFileUtils.sanitizeFileName("谢谢.xml"), "Non ISO-8859-1 characters are stripped");
         assertEquals("aaaa.xml", MardukFileUtils.sanitizeFileName("aa\naa.xml"),"New line characters are stripped");
         assertEquals("aaaa.xml", MardukFileUtils.sanitizeFileName("aa\raa.xml"),"Carriage return characters are stripped");
         assertEquals("aaaa.xml", MardukFileUtils.sanitizeFileName("aa\taa.xml"), "Tab characters are stripped");
-
     }
 }
