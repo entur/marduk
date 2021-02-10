fe9b9df2f01903212da059e41e3b4161033d6496
vincent <vincent.paturet@entur.org>
2021-02-10 12:55:45 +0100

    Auth0 migration

--- src/main/java/no/rutebanken/marduk/security/oauth2/RorAuth0RolesClaimAdapter.java
+++ src/main/java/no/rutebanken/marduk/security/oauth2/RorAuth0RolesClaimAdapter.java
@@ -1,26 +1,47 @@
 package no.rutebanken.marduk.security.oauth2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.oauth2.jwt.MappedJwtClaimSetConverter;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 
 
 /**
- * Insert a "roles" claim in the JWT token based on the organisationID claim, for compatibility with the existing
- * authorization process (@{@link JwtRoleAssignmentExtractor}).
+ * Insert a "roles" and "realm_access" claims in the JWT token based on the namespaced "roles" claim and the "permissions" claim, for compatibility with the existing
+ * authorization process (@{@link no.rutebanken.marduk.security.oauth2.JwtRoleAssignmentExtractor}).
  */
 @Component
 public class RorAuth0RolesClaimAdapter implements Converter<Map<String, Object>, Map<String, Object>> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RorAuth0RolesClaimAdapter.class);
+
     private final MappedJwtClaimSetConverter delegate =
             MappedJwtClaimSetConverter.withDefaults(Collections.emptyMap());
 
+    @Value("${marduk.oauth2.resourceserver.auth0.ror.claim.namespace}")
+    private String rorAuth0ClaimNamespace;
+
     @Override
     public Map<String, Object> convert(Map<String, Object> claims) {
-        return this.delegate.convert(claims);
+        Map<String, Object> convertedClaims = this.delegate.convert(claims);
+
+        Object permissions = convertedClaims.get("permissions");
+        if(permissions != null) {
+            convertedClaims.put("realm_access", Map.of("roles", permissions));
+        }
+
+        Object roles = convertedClaims.get(rorAuth0ClaimNamespace + "roles");
+        if(roles != null) {
+            convertedClaims.put("roles", roles);
+        }
+
+        return convertedClaims;
     }
 
 
