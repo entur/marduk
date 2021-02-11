1062deae6789d9d7a951e75eed82cfe1b8cd4b25
vincent <vincent.paturet@entur.org>
2021-02-11 11:01:19 +0100

    Auth0 migration

--- src/main/java/no/rutebanken/marduk/security/oauth2/RorAuth0RolesClaimAdapter.java
+++ src/main/java/no/rutebanken/marduk/security/oauth2/RorAuth0RolesClaimAdapter.java
@@ -1,25 +1,20 @@
 package no.rutebanken.marduk.security.oauth2;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.oauth2.jwt.MappedJwtClaimSetConverter;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 
 
 /**
- * Insert a "roles" and "realm_access" claims in the JWT token based on the namespaced "roles" and "role_assignments" claims, for compatibility with the existing
+ * Insert a "roles" and "realm_access" claims in the JWT token based on the "permission" claim and the  namespaced "roles" and "role_assignments" claims, for compatibility with the existing
  * authorization process (@{@link no.rutebanken.marduk.security.oauth2.JwtRoleAssignmentExtractor}).
  */
 @Component
 public class RorAuth0RolesClaimAdapter implements Converter<Map<String, Object>, Map<String, Object>> {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(RorAuth0RolesClaimAdapter.class);
 
     private final MappedJwtClaimSetConverter delegate =
             MappedJwtClaimSetConverter.withDefaults(Collections.emptyMap());
@@ -31,14 +26,22 @@ public class RorAuth0RolesClaimAdapter implements Converter<Map<String, Object>,
     public Map<String, Object> convert(Map<String, Object> claims) {
         Map<String, Object> convertedClaims = this.delegate.convert(claims);
 
+        // roles assigned to a user are found in the namespaced "roles" claim
         Object roles = convertedClaims.get(rorAuth0ClaimNamespace + "roles");
-        if(roles != null) {
+        if (roles != null) {
             convertedClaims.put("realm_access", Map.of("roles", roles));
+        } else {
+            // roles assigned to an application (machine-to-machine service call) are found in the "permissions" claim
+            Object permissions = convertedClaims.get("permissions");
+            if (permissions != null) {
+                convertedClaims.put("realm_access", Map.of("roles", permissions));
+            }
         }
 
-        Object rolesAssignments = convertedClaims.get(rorAuth0ClaimNamespace + "role_assignments");
-        if(rolesAssignments != null) {
-            convertedClaims.put("roles", rolesAssignments);
+        // role assignments for users and applications are found in the namespaced "role_assignments" claim
+        Object roleAssignments = convertedClaims.get(rorAuth0ClaimNamespace + "role_assignments");
+        if (roleAssignments != null) {
+            convertedClaims.put("roles", roleAssignments);
         }
 
         return convertedClaims;
