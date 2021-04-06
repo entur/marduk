16b5bc9d5785e5756dc6ba498a15410d2ddac7c1
vincent <vincent.paturet@entur.org>
2021-04-06 12:21:28 +0200

    Refactor date time API

--- src/main/java/no/rutebanken/marduk/json/ObjectMapperFactory.java
+++ src/main/java/no/rutebanken/marduk/json/ObjectMapperFactory.java
@@ -3,9 +3,12 @@ package no.rutebanken.marduk.json;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-public class ObjectMapperFactory {
+public final class ObjectMapperFactory {
 
     private static final ObjectMapper SHARED_OBJECT_MAPPER = new ObjectMapper().registerModule(new JavaTimeModule());
+
+    private ObjectMapperFactory() {
+    }
 
     public static ObjectMapper getSharedObjectMapper() {
         return SHARED_OBJECT_MAPPER;
