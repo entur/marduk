a421faf1b390dbe39ba4f4dd3c6cae8b59a0e4ff
vincent <vincent.paturet@entur.org>
2021-11-17 10:30:41 +0100

    Added monitoring of Antu NeTEx validation

---
+++ src/main/java/no/rutebanken/marduk/routes/chouette/AntuNetexValidationStatusRouteBuilder.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ *   https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ *
+ */
+
+package no.rutebanken.marduk.routes.chouette;
+
+import org.apache.camel.LoggingLevel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AntuNetexValidationStatusRouteBuilder extends AbstractChouetteRouteBuilder {
+
+    private static final String DATASET_CODESPACE = "EnturDatasetCodespace";
+
+    private static final String STATUS_EXPORT_STARTED = "started";
+    private static final String STATUS_EXPORT_OK = "ok";
+    private static final String STATUS_EXPORT_FAILED = "failed";
+
+    @Override
+    public void configure() throws Exception {
+        super.configure();
+
+        from("entur-google-pubsub:AntuNetexValidationStatusQueue")
+                .process(this::setCorrelationIdIfMissing)
+                .choice()
+                .when(body().isEqualTo(constant(STATUS_EXPORT_STARTED)))
+                .to("direct:antuNetexValidationStarted")
+                .when(body().isEqualTo(constant(STATUS_EXPORT_OK)))
+                .to("direct:antuNetexValidationComplete")
+                .when(body().isEqualTo(constant(STATUS_EXPORT_FAILED)))
+                .to("direct:antuNetexValidationFailed")
+                .routeId("antu-netex-validation-status");
+
+        from("direct:antuNetexValidationStarted")
+                .log(LoggingLevel.INFO, getClass().getName(), correlation() + "Antu NeTEx validation started for codespace ${header." + DATASET_CODESPACE + "}")
+                .routeId("antu-netex-validation-started");
+
+        from("direct:antuNetexValidationComplete")
+                .log(LoggingLevel.INFO, getClass().getName(), correlation() + "Antu NeTEx validation complete for codespace ${header." + DATASET_CODESPACE + "}")
+                .routeId("antu-netex-validation-complete");
+
+        from("direct:antuNetexValidationFailed")
+                .log(LoggingLevel.INFO, getClass().getName(), correlation() + "Antu NeTEx validation failed for codespace ${header." + DATASET_CODESPACE + "}")
+                .routeId("antu-netex-validation-failed");
+
+    }
+}
