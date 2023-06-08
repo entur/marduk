9a709fb4fa675059ae22fdbd8cb576922fe5c03f
Assad Riaz <assad.riaz@entur.org>
2023-06-08 14:04:26 +0200

    terraform config refactoring

---
+++ terraform-gcp2/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 0.13.2"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.32.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.17.0"
+    }
+  }
+}
