ee27c077bcb184f3916be4feb99be1111e29f126
Vincent Paturet <vincent.paturet@entur.org>
2023-10-04 10:27:20 +0200

    Update Terraform Google provider

--- terraform-gcp2/providers.tf
+++ terraform-gcp2/providers.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.32.0"
+      version = "~> 4.84.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
