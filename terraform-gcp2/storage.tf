633459431ec99115d4fa9acf257e31fdd56bd3e2
Vincent Paturet <vincent.paturet@entur.org>
2023-10-05 13:09:17 +0200

    Add lifecycle rule for internal bucket work directory

--- terraform-gcp2/storage.tf
+++ terraform-gcp2/storage.tf
@@ -13,7 +13,7 @@ resource "google_storage_bucket" "storage_bucket" {
   }
   lifecycle_rule {
       condition {
-        matches_prefix = "graphs/work"
+        matches_prefix = ["graphs/work"]
         age = 1
         with_state = "ANY"
       }
