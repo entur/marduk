f617a6575108691f2e1d9ff047710830d2afbce6
Vincent Paturet <46598384+vpaturet@users.noreply.github.com>
2024-06-04 15:08:10 +0200

    Extract AuthorizationService interface (#601)
    * Extract AuthorizationService interface
    
    * Refactor unit test

--- src/main/java/no/rutebanken/marduk/security/AuthorizationService.java
+++ src/main/java/no/rutebanken/marduk/security/AuthorizationService.java
@@ -1,107 +1,25 @@
-/*
- * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
- * the European Commission - subsequent versions of the EUPL (the "Licence");
- * You may not use this work except in compliance with the Licence.
- * You may obtain a copy of the Licence at:
- *
- *   https://joinup.ec.europa.eu/software/page/eupl
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the Licence is distributed on an "AS IS" basis,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Licence for the specific language governing permissions and
- * limitations under the Licence.
- *
- */
-
 package no.rutebanken.marduk.security;
 
-import no.rutebanken.marduk.domain.Provider;
-import no.rutebanken.marduk.repository.ProviderRepository;
-import org.rutebanken.helper.organisation.AuthorizationConstants;
-import org.rutebanken.helper.organisation.RoleAssignment;
-import org.rutebanken.helper.organisation.RoleAssignmentExtractor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Service;
+public interface AuthorizationService {
 
-import java.util.List;
-
-@Service
-public class AuthorizationService {
-
-    @Autowired
-    private ProviderRepository providerRepository;
-
-    @Autowired
-    private RoleAssignmentExtractor roleAssignmentExtractor;
-
-
-    @Value("${authorization.enabled:true}")
-    protected boolean authorizationEnabled;
-
-    public void verifyAdministratorPrivileges() {
-        verifyAtLeastOne(new AuthorizationClaim(AuthorizationConstants.ROLE_ROUTE_DATA_ADMIN));
-    }
 
     /**
+     * Verify that the user has full administrator privileges.
+     */
+    void verifyAdministratorPrivileges();
+
+    /**
+     * Verify that the user can edit route data for a given provider.
      * Users can edit route data if they have administrator privileges,
      * or if it has editor privileges for this provider.
-     * @param providerId
-     */    public void verifyRouteDataEditorPrivileges(Long providerId) {
-        verifyAtLeastOne(new AuthorizationClaim(AuthorizationConstants.ROLE_ROUTE_DATA_ADMIN),
-                new AuthorizationClaim(AuthorizationConstants.ROLE_ROUTE_DATA_EDIT, providerId));
-    }
+     */
+    void verifyRouteDataEditorPrivileges(Long providerId);
 
     /**
+     * Verify that the user can read block data for a given provider.
      * Users can download NeTEx blocks data if they have administrator privileges,
      * or if they have editor privileges for this provider
      * or if they have NeTEx blocks viewer privileges for this provider.
-     * @param providerId
      */
-    public void verifyBlockViewerPrivileges(Long providerId) {
-        verifyAtLeastOne(new AuthorizationClaim(AuthorizationConstants.ROLE_ROUTE_DATA_ADMIN),
-                new AuthorizationClaim(AuthorizationConstants.ROLE_ROUTE_DATA_EDIT, providerId),
-                new AuthorizationClaim(AuthorizationConstants.ROLE_NETEX_BLOCKS_DATA_VIEW, providerId));
-    }
-
-    protected void verifyAtLeastOne(AuthorizationClaim... claims) {
-        if (!authorizationEnabled){
-            return;
-        }
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        List<RoleAssignment> roleAssignments = roleAssignmentExtractor.getRoleAssignmentsForUser(authentication);
-
-        boolean authorized = false;
-        for (AuthorizationClaim claim : claims) {
-            if (claim.getProviderId() == null) {
-                authorized |= roleAssignments.stream().anyMatch(ra -> claim.getRequiredRole().equals(ra.getRole()));
-            } else {
-                authorized |= hasRoleForProvider(roleAssignments, claim);
-            }
-        }
-
-        if (!authorized) {
-            throw new AccessDeniedException("Insufficient privileges for operation");
-        }
-
-    }
-
-
-    private boolean hasRoleForProvider(List<RoleAssignment> roleAssignments, AuthorizationClaim claim) {
-
-        Provider provider = providerRepository.getProvider(claim.getProviderId());
-        if (provider == null) {
-            return false;
-        }
-
-        return roleAssignments.stream()
-                       .filter(ra -> claim.getRequiredRole().equals(ra.r)).anyMatch(ra ->  provider.getChouetteInfo().getReferential().toUpperCase().equals(ra.o));
-
-    }
-
+    void verifyBlockViewerPrivileges(Long providerId);
 }
