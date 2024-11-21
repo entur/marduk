4196a3cd455cad340838ef9d39e850f1a05c63fd
Vincent Paturet <46598384+vpaturet@users.noreply.github.com>
2024-11-21 16:12:17 +0100

    Add support for Finland dataset validation (#631)

--- src/main/java/no/rutebanken/marduk/Constants.java
+++ src/main/java/no/rutebanken/marduk/Constants.java
@@ -128,6 +128,7 @@ public final class Constants {
 
     public static final String VALIDATION_PROFILE_TIMETABLE_FLEX_MERGING = "TimetableFlexibleTransportMerging";
     public static final String VALIDATION_PROFILE_TIMETABLE_SWEDEN = "TimetableSweden";
+    public static final String VALIDATION_PROFILE_TIMETABLE_FINLAND = "TimetableFinland";
 
     public static final String IMPORT_TYPE = "ImportType";
 
