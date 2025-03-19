123ab8856a65dcd73a60910f24ddef3fa12449fc
erlendev <erlend.vige@gmail.com>
2025-03-19 10:28:40 +0100

    Ensures we use feature-toggle for export codespace GTFS route

--- src/main/java/no/rutebanken/marduk/routes/netex/PublishMergedNetexRouteBuilder.java
+++ src/main/java/no/rutebanken/marduk/routes/netex/PublishMergedNetexRouteBuilder.java
@@ -72,12 +72,13 @@ public class PublishMergedNetexRouteBuilder extends BaseRouteBuilder {
                 .removeHeader(DATASET_REFERENTIAL)
                 .setBody(header(CHOUETTE_REFERENTIAL))
                 .process(this::removeAllCamelHeaders)
-                .setHeader(GTFS_ROUTE_DISPATCHER_HEADER_NAME, simple(GTFS_ROUTE_DISPATCHER_EXPORT_HEADER_VALUE))
-                .to("google-pubsub:{{marduk.pubsub.project.id}}:GtfsRouteDispatcherTopic")
+                .choice()
+                    .when(simple("${properties:marduk.gtfs-aggregation-next.enabled} == 'true'"))
+                        .setHeader(GTFS_ROUTE_DISPATCHER_HEADER_NAME, simple(GTFS_ROUTE_DISPATCHER_EXPORT_HEADER_VALUE))
+                        .to("google-pubsub:{{marduk.pubsub.project.id}}:GtfsRouteDispatcherTopic")
+                    .otherwise()
+                        .to("google-pubsub:{{marduk.pubsub.project.id}}:DamuExportGtfsQueue")
+                .end()
                 .routeId("start-damu-gtfs-export");
-
-
-
-
     }
 }
