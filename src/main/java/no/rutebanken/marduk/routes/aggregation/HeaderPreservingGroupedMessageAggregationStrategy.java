89819d9d388aaf41ed9d8d9e1573133780a468e8
erlendev <erlend.vige@gmail.com>
2025-03-21 12:06:57 +0100

    Writes test for header aggregation strategy

--- src/main/java/no/rutebanken/marduk/routes/aggregation/HeaderPreservingGroupedMessageAggregationStrategy.java
+++ src/main/java/no/rutebanken/marduk/routes/aggregation/HeaderPreservingGroupedMessageAggregationStrategy.java
@@ -3,6 +3,7 @@ package no.rutebanken.marduk.routes.aggregation;
 import org.apache.camel.Exchange;
 import org.apache.camel.processor.aggregate.GroupedMessageAggregationStrategy;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,17 +17,20 @@ public class HeaderPreservingGroupedMessageAggregationStrategy extends GroupedMe
     @Override
     public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
         Exchange aggregatedExchange = super.aggregate(oldExchange, newExchange);
-        copyHeadersForPreservation(newExchange, aggregatedExchange);
+        Map<String, Object> aggregatedHeaders = copyHeadersForPreservation(newExchange);
+        aggregatedExchange.getIn().setHeaders(aggregatedHeaders);
         return aggregatedExchange;
     }
 
-    private void copyHeadersForPreservation(Exchange newExchange, Exchange aggregatedExchange) {
+    protected Map<String, Object> copyHeadersForPreservation(Exchange newExchange) {
         Map<String, Object> headers = newExchange.getIn().getHeaders();
+        Map<String, Object> preservedHeaders = new HashMap<>();
         for (String headerName : headersToPreserve) {
             Object headerValue = headers.get(headerName);
             if (headerValue != null) {
-                aggregatedExchange.getIn().setHeader(headerName, headerValue);
+                preservedHeaders.put(headerName, headerValue);
             }
         }
+        return preservedHeaders;
     }
 }
