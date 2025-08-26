ba4ffe93d95bc5a98d90d07cbbf4df214d4ebce3
erlendev <erlend.vige@gmail.com>
2025-08-26 15:29:01 +0200

    Adds ashur pubsub to terraform

--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -407,3 +407,41 @@ resource "google_pubsub_topic" "LineStatisticsCalculationQueue" {
   project = var.gcp_resources_project
   labels = var.labels
 }
+
+resource "google_pubsub_topic" "FilterNetexFileQueue" {
+  name    = "FilterNetexFileQueue"
+  project = var.gcp_resources_project
+  labels  = var.labels
+}
+
+resource "google_pubsub_subscription" "FilterNetexFileQueue" {
+  name                       = "FilterNetexFileQueue"
+  topic                      = google_pubsub_topic.FilterNetexFileQueue.name
+  project                    = var.gcp_resources_project
+  labels                     = var.labels
+  message_retention_duration = "3600s"
+  ack_deadline_seconds       = 600
+  expiration_policy {
+    minimum_backoff = "10s"
+    maximum_backoff = "600s"
+  }
+}
+
+resource "google_pubsub_topic" "FilterNetexFileStatusQueue" {
+  name    = "FilterNetexFileStatusQueue"
+  project = var.gcp_resources_project
+  labels  = var.labels
+}
+
+resource "google_pubsub_subscription" "FilterNetexFileStatusQueue" {
+  name                       = "FilterNetexFileStatusQueue"
+  topic                      = google_pubsub_topic.FilterNetexFileStatusQueue.name
+  project                    = var.gcp_resources_project
+  labels                     = var.labels
+  message_retention_duration = "3600s"
+  ack_deadline_seconds       = 30
+  expiration_policy {
+    minimum_backoff = "10s"
+    maximum_backoff = "600s"
+  }
+}
