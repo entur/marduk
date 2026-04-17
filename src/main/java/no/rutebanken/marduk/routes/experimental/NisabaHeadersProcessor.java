6b9b8fe0fdb47fe203b50927bac9e93e7657e42a
erlendev <erlend.vige@gmail.com>
2026-04-17 12:06:13 +0200

    Aligns timestamps in CompositeFrame and nisaba
    Fixes a regression in the experimental import, which resulted in nisaba failing in matching it's netex .zip file with the aggregated netex file placed in marduk's outbound bucket for that same codespace and import process. The issue was caused by timestamps not being synced between the two when using the experimental import:
    * when naming the .zip file copied to nisaba-exchange, the experimental import would use LocalDateTime.now() in the resulting zip file name
    * when producing CompositeFrame elements through Ashur, the experimental import would use the createdAt value from the idempotentRepository for the file being imported
    
    This commit fixes this inconsistency by using the same createdAt timestamp as Ashur for naming the zip file copied to nisaba-exchange
    
    Not directly related to this commit, but important to note:
    The timestamps in the zip file names copied to nisaba use millisecond precision. When producing this timestamp, it is important to ensure that microsecond precision is truncated from the timestamp, rather than having the millisecond be rounded up or down. This is already handled in a correct way by marduk.

--- src/main/java/no/rutebanken/marduk/routes/experimental/NisabaHeadersProcessor.java
+++ src/main/java/no/rutebanken/marduk/routes/experimental/NisabaHeadersProcessor.java
@@ -3,9 +3,13 @@ package no.rutebanken.marduk.routes.experimental;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoField;
 
 import static no.rutebanken.marduk.Constants.*;
@@ -14,6 +18,7 @@ import static no.rutebanken.marduk.Constants.*;
  * Processor used to set headers for files being uploaded to the Nisaba exchange bucket.
  * */
 public class NisabaHeadersProcessor implements Processor {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NisabaHeadersProcessor.class);
     private final String containerName;
 
     public NisabaHeadersProcessor(String containerName) {
@@ -26,10 +31,29 @@ public class NisabaHeadersProcessor implements Processor {
                 .appendPattern("yyyy-MM-dd'T'HH:mm:ss")
                 .appendFraction(ChronoField.NANO_OF_SECOND, 0, 3, true)
                 .toFormatter();
-        String timestamp = LocalDateTime.now().format(fmt).replace(":", "_");
-        String targetFileName = "/" + exchange.getIn().getHeader(CHOUETTE_REFERENTIAL, String.class) + "_" + timestamp + ".zip";
-        String pathToTargetFile = "imported/" + exchange.getIn().getHeader(CHOUETTE_REFERENTIAL, String.class) + targetFileName;
+        LocalDateTime dateTime = resolveTimestamp(exchange);
+        String timestamp = dateTime.format(fmt).replace(":", "_");
+        String referential = exchange.getIn().getHeader(CHOUETTE_REFERENTIAL, String.class);
+        String targetFileName = "/" + referential + "_" + timestamp + ".zip";
+        String pathToTargetFile = "imported/" + referential + targetFileName;
         exchange.getIn().setHeader(TARGET_FILE_HANDLE, pathToTargetFile);
         exchange.getIn().setHeader(TARGET_CONTAINER, containerName);
+        LOGGER.info("Set Nisaba upload target: container='{}', fileHandle='{}'", containerName, pathToTargetFile);
+    }
+
+    private LocalDateTime resolveTimestamp(Exchange exchange) {
+        String headerValue = exchange.getIn().getHeader(FILTERING_FILE_CREATED_TIMESTAMP, String.class);
+        if (headerValue != null) {
+            try {
+                LocalDateTime parsed = LocalDateTime.parse(headerValue);
+                LOGGER.info("Using timestamp from {} header: {}", FILTERING_FILE_CREATED_TIMESTAMP, parsed);
+                return parsed;
+            } catch (DateTimeParseException e) {
+                LOGGER.warn("Failed to parse {} header value '{}', falling back to current time", FILTERING_FILE_CREATED_TIMESTAMP, headerValue, e);
+            }
+        } else {
+            LOGGER.info("No {} header present, falling back to current time", FILTERING_FILE_CREATED_TIMESTAMP);
+        }
+        return LocalDateTime.now();
     }
 }
